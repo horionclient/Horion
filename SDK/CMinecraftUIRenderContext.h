079b8632f3044324d51b5cf7a3fdaf6f568268a8
godgamer029 <godgamer029@gmail.com>
2019-05-01 21:45:32 +0200

    yeet

--- SDK/CMinecraftUIRenderContext.h
+++ SDK/CMinecraftUIRenderContext.h
@@ -5,7 +5,7 @@
 class C_MinecraftUIRenderContext {
 public:
 	virtual ~C_MinecraftUIRenderContext();
-	virtual __int64 getLineLength(__int64 a1, __int64 a2, __int64 a3, __int64 a4, __int64 a5);
+	virtual __int64 getLineLength(uintptr_t font, TextHolder* str, float textSize, bool idk);
 	virtual float getTextAlpha();
 	virtual void setTextAlpha(float alpha);
 	virtual __int64 drawDebugText(const float* pos, TextHolder* text, float* color, float alpha, unsigned int textAlignment, const float* textMeasureData, const void* caretMeasureData);
