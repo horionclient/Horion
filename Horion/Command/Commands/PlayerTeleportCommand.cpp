0bb9ffb37f37c802a6f14bbb9c0eaa38d604ea16
godgamer029 <godgamer029@gmail.com>
2019-05-27 17:16:17 +0200

    chungus

--- Horion/Command/Commands/PlayerTeleportCommand.cpp
+++ Horion/Command/Commands/PlayerTeleportCommand.cpp
@@ -16,28 +16,27 @@ PlayerTeleportCommand::~PlayerTeleportCommand()
 bool PlayerTeleportCommand::execute(std::vector<std::string>* args)
 {
 	assertTrue(g_Data.getClientInstance()->getLocalPlayer() != nullptr);
-	assertTrue(args->size() >= 2);
+	assertTrue(args->size() > 1); // .playertp <player>
 	std::string nameOfPlayer = args->at(1);
 	assertTrue(nameOfPlayer.size() > 0);
 
 	std::transform(nameOfPlayer.begin(), nameOfPlayer.end(), nameOfPlayer.begin(), ::tolower);
 
-	C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
-	C_Entity* currentEntity;
+	
 
 	C_EntityList* entList = g_Data.getEntityList();
 	size_t listSize = entList->getListSize();
 	vec3_t pos;
 
-	if (listSize > 1000) {
+	if (listSize > 5000) {
 		logF("Big ent list wtf men %i", listSize);
 		return true;
 	}
-
+	const char* playerName = nullptr;
 	//Loop through all our players and retrieve their information
 	for (size_t i = 0; i < listSize; i++)
 	{
-		currentEntity = entList->get(i);
+		C_Entity* currentEntity = entList->get(i);
 
 		std::string name(currentEntity->name2.getText());
 
@@ -47,15 +46,16 @@ bool PlayerTeleportCommand::execute(std::vector<std::string>* args)
 			continue;
 
 		pos = currentEntity->eyePos0;
+		playerName = name.c_str();
 		break;
 
 	}
-	if (pos.iszero())
+	if (pos.iszero() || playerName == nullptr)
 	{
-		clientMessageF("[%sHorion%s] %sCouldn't find player : %s !",GOLD,WHITE,GREEN,nameOfPlayer.c_str());
+		clientMessageF("[%sHorion%s] %sCouldn't find player: %s!",GOLD,WHITE,RED,nameOfPlayer.c_str());
 		return true;
 	}
 	g_Data.getLocalPlayer()->setPos(pos);
-	clientMessageF("[%sHorion%s] %sTeleported to %s",GOLD,WHITE,GREEN,currentEntity->name2.getText());
+	clientMessageF("[%sHorion%s] %sTeleported to %s",GOLD,WHITE,GREEN, playerName);
 	return true;
 }
