ce9d8543ddaf415e2ea3d58ebe4b2f579bd54efc
godgamer029 <godgamer029@gmail.com>
2019-05-27 17:07:58 +0200

    Friendlist

--- Horion/Command/Commands/FriendListCommand.cpp
+++ Horion/Command/Commands/FriendListCommand.cpp
@@ -2,9 +2,9 @@
 
 
 
-FriendListCommand::FriendListCommand() : ICommand("friend", "add/remove friends", "<add/remove>")
+FriendListCommand::FriendListCommand() : ICommand("friend", "Add/Remove friendly players", "<add/remove>")
 {
-	registerAlias("array");
+	registerAlias("friendlist");
 }
 
 
@@ -15,52 +15,57 @@ FriendListCommand::~FriendListCommand()
 
 bool FriendListCommand::execute(std::vector<std::string>* args)
 {
-	assertTrue(args->size() >= 1);
 	assertTrue(args->size() >= 3);
-	std::string yeet = args->at(1);
-	std::string name = args->at(2);
-	std::transform(name.begin(), name.end(), name.begin(), ::tolower);
-
-	C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
-	C_Entity* currentEntity;
+	std::string subcommand = args->at(1);
+	std::transform(subcommand.begin(), subcommand.end(), subcommand.begin(), ::tolower);
+	std::string searchedName = args->at(2); // Friend to add/remove
+	std::transform(searchedName.begin(), searchedName.end(), searchedName.begin(), ::tolower); // tolower
 
 	C_EntityList* entList = g_Data.getEntityList();
 	size_t listSize = entList->getListSize();
 
-	if (listSize > 1000) {
+	if (listSize > 10000) {
 		logF("Big ent list wtf men %i", listSize);
 		return true;
 	}
 
+	std::string playerName = nullptr;
 	//Loop through all our players and retrieve their information
 	for (size_t i = 0; i < listSize; i++)
 	{
-		currentEntity = entList->get(i);
+		C_Entity* currentEntity = entList->get(i);
 
-		std::string Orig_name(currentEntity->name2.getText());
+		std::string currentEntityName(currentEntity->name2.getText());
 
-		std::transform(Orig_name.begin(), Orig_name.end(), Orig_name.begin(), ::tolower);
+		std::transform(currentEntityName.begin(), currentEntityName.end(), currentEntityName.begin(), ::tolower); // tolower
 
-		if (Orig_name.find(name) == std::string::npos)
+		if (currentEntityName.find(searchedName) == std::string::npos) // Continue if name not found
 			continue;
 
+		playerName = currentEntityName;
 		break;
 
 	}
-	if (yeet == "add")
+	if (subcommand == "add")
 	{
-		FriendList::addPlayerToList(currentEntity->name2.getText());
-		clientMessageF("[%sHorion%s] %s%s is now your friend !", GOLD, WHITE, GREEN, currentEntity->name2.getText());
+		FriendList::addPlayerToList(playerName);
+		clientMessageF("[%sHorion%s] %s%s is now your friend!", GOLD, WHITE, GREEN, playerName);
 		return true;
 
 	}
-	else if (yeet == "remove" && FriendList::removePlayer(name))
+	else if (subcommand == "remove")
 	{
-		clientMessageF("[%sHorion%s] %s%s is not your friend anymore :( !", GOLD, WHITE, GREEN,name.c_str());
-		return true;
+		if (FriendList::removePlayer(searchedName)) {
+			clientMessageF("[%sHorion%s] %s%s has been removed from your friendlist!", GOLD, WHITE, GREEN, searchedName.c_str());
+			return true;
+		}
+		else {
+			clientMessageF("[%sHorion%s] %s%s was not in your friendlist!", GOLD, WHITE, GREEN, searchedName.c_str());
+			return true;
+		}
 	}
 
-	clientMessageF("[%sHorion%s] %sUnknow Command!", GOLD, WHITE, GREEN);
-	return true;
+	
+	return false;
 }
 
