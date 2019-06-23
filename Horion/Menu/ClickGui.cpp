8ecb068164c50f3cee6263f1915409e7a5de15f4
godgamer029 <godgamer029@gmail.com>
2019-06-23 15:00:09 +0200

    idk

--- Horion/Menu/ClickGui.cpp
+++ Horion/Menu/ClickGui.cpp
@@ -1,10 +1,10 @@
 #include "ClickGui.h"
 
 // Render
-static float yOffset;
-static float xOffset;
 
-bool isdown;
+
+bool isLeftClickDown = false;
+bool isRightClickDown = false;
 
 void ClickGui::getModuleListByCategory(Category category, std::vector<IModule*>* modList) {
 	std::vector<IModule*>* moduleList = moduleMgr->getModuleList();
@@ -20,6 +20,9 @@ void ClickGui::renderCategory(Category category)
 	static constexpr float textPadding = 1.0f;
 	static constexpr float textSize = 1.0f;
 	static constexpr float textHeight = textSize * 10.0f;
+
+	float yOffset = 4;
+	float xOffset = 150;
 
 	const char* categoryName;
 	switch (category) {
@@ -99,9 +102,9 @@ void ClickGui::renderCategory(Category category)
 		if (!GameData::canUseMoveKeys() && rectPos.contains(&mousePos)) {
 
 			DrawUtils::fillRectangle(rectPos, MC_Color(0.4f, 0.9f, 0.4f, 0.1f),1.0f);
-			if (isdown) {
+			if (isLeftClickDown) {
 				(*it)->toggle();
-				isdown = false;
+				isLeftClickDown = false;
 			}
 		}
 		GuiUtils::drawCrossLine(vec4_t(rectPos.z - 8.0f, rectPos.y + 1.0f, rectPos.z - 1.0f, rectPos.w - 1.0f), MC_Color(1.0f, 0.2f, 0, 1.0f), 0.5f, true);
@@ -111,7 +114,7 @@ void ClickGui::renderCategory(Category category)
 	DrawUtils::fillRectangle(vec4_t(xOffset, 4, rectPos.z,yOffset), MC_Color(0.f, 0.1f, 0.1f, 0.1f), 0.4f);
 	DrawUtils::flush();
 	moduleList.clear();
-	yOffset = 4;
+	
 	xOffset += 100;
 }
 
@@ -119,22 +122,21 @@ void ClickGui::render()
 {
 	if (!moduleMgr->isInitialized())
 		return;
-	yOffset = 4;
-	xOffset = 150;
 	// Render all categorys
 	renderCategory(COMBAT);
 	renderCategory(VISUAL);
 	renderCategory(MOVEMENT);
 	renderCategory(BUILD);
 	renderCategory(EXPLOITS);
+
+	isLeftClickDown = false;
+	isRightClickDown = false;
 }
 
 void ClickGui::init() {
-	yOffset = 4;
-	xOffset = 100;
 }
 
 void ClickGui::onMouseClickUpdate(bool isDown)
 {
-	isdown = isDown;
+	isLeftClickDown = isDown;
 }
