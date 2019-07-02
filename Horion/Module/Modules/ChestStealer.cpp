e720428871d52f53fd138100e002eb1159ba0697
erwan celanie <ti-yoyo789@hotmail.fr>
2019-07-02 18:05:39 -0400

    clean

--- Horion/Module/Modules/ChestStealer.cpp
+++ Horion/Module/Modules/ChestStealer.cpp
@@ -20,12 +20,11 @@ const char* ChestStealer::getModuleName()
 void ChestStealer::onTick(C_GameMode* gm)
 {
 	C_ChestScreenController** chestScreenController = g_Data.getChestScreenController();
-	uintptr_t idk = 0x7FFFFFFF;
 	if (g_Data.getLocalPlayer()->isInventoryClosed() == 0 && *chestScreenController != NULL)
 	{ 
 		for (int i = 0; i < 54; i++)
 		{
-			(*chestScreenController)->handleAutoPlace(idk,"container_items", i);
+			(*chestScreenController)->handleAutoPlace(0x7FFFFFFF,"container_items", i);
 		}
 	}
 	*chestScreenController = NULL;
