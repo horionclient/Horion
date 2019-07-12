be1d689090a2d4b76f5d7bc4221af8a08b873aed
erwan celanie <ti-yoyo789@hotmail.fr>
2019-07-12 04:09:03 -0400

    hitbox check

--- Utils/Target.cpp
+++ Utils/Target.cpp
@@ -1,4 +1,5 @@
 #include "Target.h"
+#include "../Horion/Module/ModuleManager.h"
 
 C_LocalPlayer** localPlayer;
 
@@ -11,14 +12,21 @@ bool Target::isValidTarget(C_Entity * ent)
 	if (ent == 0)
 		return false;
 
+	static Hitbox* hitboxMod = static_cast<Hitbox*>(moduleMgr->getModule<Hitbox>());
+	if (hitboxMod == 0)
+		hitboxMod = static_cast<Hitbox*>(moduleMgr->getModule<Hitbox>());
+
 	const bool isPlayer = ent->getEntityTypeId() == 63;
 
 	if (isPlayer)
 		return false;
 	if (ent->getNameTag()->getTextLength() <= 1)
 		return false;
-	if (ent->height < 1.5f || ent->width < 0.5f || ent->height > 2.1f || ent->width > 0.9f)
-		return false;
+
+	if(!hitboxMod->isEnabled())
+		if (ent->height < 1.5f || ent->width < 0.5f || ent->height > 2.1f || ent->width > 0.9f)
+			return false;
+
 	if (ent->isInvisible() && ent->getEntityTypeId() != 33) // Exception for kitmap.sylphhcf.net they use a creeper as hitbox
 		return false;
 	if (!(*localPlayer)->canAttack(ent, false))
