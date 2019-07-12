e0ab230f49974a2106040013fbc9997d2befc6b5
erwan celanie <ti-yoyo789@hotmail.fr>
2019-07-12 12:48:12 -0400

    fixed reach

--- Horion/Module/Modules/Reach.cpp
+++ Horion/Module/Modules/Reach.cpp
@@ -18,15 +18,33 @@ const char* Reach::getModuleName()
 
 void  Reach::onTick(C_GameMode* gm)
 {
+	if (reachPtr != 0) {
+		*reachPtr = reachValue;
+	}
+
+}
+
+
+void Reach::onEnable()
+{
 	static uintptr_t sigOffset = 0x0;
 	if (sigOffset == 0x0) {
 		sigOffset = Utils::FindSignature("EB 18 F3 0F 10 05 ?? ?? ?? ?? 44 0F 2F D0");
 		if (sigOffset != 0x0) {
 			int offset = *reinterpret_cast<int*>((sigOffset + 6)); // Get Offset from code
-			reachPtr = reinterpret_cast<float*>(sigOffset  + offset + 10); 
-			logF("reachPtr: %llX", reachPtr);
+			reachPtr = reinterpret_cast<float*>(sigOffset + offset + 10);
 		}
+	}
+	if (!VirtualProtect(reachPtr, sizeof(float), PAGE_EXECUTE_READWRITE, &oldProtect)) {
+#ifdef _DEBUG
+		logF("couldnt unprotect memory send help");
+		__debugbreak();
+#endif
 	}
 }
 
-
+void Reach::onDisable()
+{
+	if (reachPtr != 0)
+		VirtualProtect(reachPtr, sizeof(float), oldProtect, &oldProtect);
+}
