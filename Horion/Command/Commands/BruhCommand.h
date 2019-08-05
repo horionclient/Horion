01df63d7453f87d79c3394f6d49b2275564ba57f
GodSoft029 <Godsoft029@users.noreply.github.com>
2019-08-05 18:24:08 +0200

    Create BruhCommand.h
    web editor sucks

---
+++ Horion/Command/Commands/BruhCommand.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ICommand.h"
+class BruhCommand :
+	public IMCCommand
+{
+public:
+	BruhCommand();
+	~BruhCommand();
+
+	// Inherited via IMCCommand
+	virtual bool execute(std::vector<std::string>* args) override;
+};
