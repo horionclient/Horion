fc61c7fc89a1ee10fad4ca42336fb700fa02ac9f
GodSoft029 <Godsoft029@users.noreply.github.com>
2019-08-05 18:30:19 +0200

    Create BruhCommand.cpp

---
+++ Horion/Command/Commands/BruhCommand.cpp
@@ -0,0 +1,23 @@
+#include "BruhCommand.h"
+
+
+
+BruhCommand::BruhCommand() : IMCCommand("bruh", "bruh moment", "bruh")
+{
+	
+}
+
+
+BruhCommand::~BruhCommand()
+{
+}
+
+
+bool BruhCommand::execute(std::vector<std::string>* args)
+{
+	assertTrue(g_Data.getClientInstance()->getLocalPlayer() != nullptr);
+	
+	
+	clientMessageF("bruh moment");
+	return true;
+}
