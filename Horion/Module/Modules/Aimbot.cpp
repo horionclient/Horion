75cc8ae01c76262d3cf81cb6754aea7218367898
zzolr <49467061+zzolr@users.noreply.github.com>
2019-08-25 12:04:58 -0700

    Update Aimbot.cpp

--- Horion/Module/Modules/Aimbot.cpp
+++ Horion/Module/Modules/Aimbot.cpp
@@ -2,10 +2,16 @@
 
 
 
-Aimbot::Aimbot() : IModule('M', COMBAT) // <-- keybin
+Aimbot::Aimbot() : IModule('M', COMBAT) // <-- keybind
 {
-	this->registerFloatSetting("smoothness", &this->smoothNess, this->smoothNess, 3.f, 100.f);
 	this->registerFloatSetting("range", &this->range, this->range, 3.f, 8.f);
+	this->registerBoolSetting("Require click", &this->click, this->click);
+	this->registerBoolSetting("only swords/axes", &this->sword, this->sword);
+	this->registerBoolSetting("vertical", &this->vertical, this->vertical);
+	this->registerIntSetting("horizontal speed", &this->horizontalspeed, this->horizontalspeed, 10, 90);
+	this->registerIntSetting("vertical speed", &this->verticalspeed, this->verticalspeed, 10, 90);
+	this->registerIntSetting("horizontal range", &this->horizontalrange, this->horizontalrange, 20, 180);
+	this->registerIntSetting("vertical range", &this->verticalrange, this->verticalrange, 20, 180);
 }
 
 
@@ -36,6 +42,8 @@ void Aimbot::onPostRender()
 		return;
 	size_t listSize = entList->getListSize();
 
+	C_Entity* lastTarget;
+
 	if (listSize > 1000) {
 #ifdef _DEBUG
 		logF("Big ent list wtf men %i", listSize);
@@ -49,7 +57,7 @@ void Aimbot::onPostRender()
 	static std::vector <C_Entity*> targetList;
 	targetList.clear();
 	for (size_t i = 0; i < listSize; i++)
-	{   
+	{
 		C_Entity* currentEntity = entList->get(i);
 
 		if (currentEntity == 0)
@@ -66,35 +74,52 @@ void Aimbot::onPostRender()
 
 		if (!(currentEntity->getNameTag()->getTextLength() > 0))
 			continue;
-		
-		if (currentEntity->height < 1.5f || currentEntity->width < 0.5f || currentEntity->height > 2.1f || currentEntity->width > 0.9f)
-			continue;
 
-		// i want to hit villagers ok
-//		if (localPlayer->entityType2 != currentEntity->entityType2)
-//			continue;
-
-		float dist = (*currentEntity->getPos()).dist(*g_Data.getLocalPlayer()->getPos());;
-
-		if (dist < range) 
-		{
+		if (currentEntity == lastTarget) {
 			targetList.push_back(currentEntity);
+			break;
+		}
+
+		if (currentEntity->height == 1.8f && currentEntity->width == 0.6f) {
+
+			// i want to hit villagers ok
+	//		if (localPlayer->entityType2 != currentEntity->entityType2)
+	//			continue;
+
+			float dist = (*currentEntity->getPos()).dist(*g_Data.getLocalPlayer()->getPos());;
+
+			if (dist < range)
+			{
+				targetList.push_back(currentEntity);
+			}
 		}
 	}
 
 	if (targetList.size() > 0)
 	{
 		std::sort(targetList.begin(), targetList.end(), CompareTargetEnArray());
-
+		std::list<C_Entity*>::iterator it;
 		vec2_t angle = origin.CalcAngle(*targetList[0]->getPos());
 		vec2_t appl = angle.sub(localPlayer->viewAngles).normAngles();
 		appl.x = -appl.x;
-		
-		appl.div(smoothNess); // Smooth dat boi
 
+		
+
+		if ((appl.x < verticalrange && appl.x > -verticalrange) && (appl.y < horizontalrange && appl.y > -horizontalrange) && GameData::canUseMoveKeys() ) {
+			
+			if (sword && !(localPlayer->itemId == 268 || localPlayer->itemId == 267 || localPlayer->itemId == 272 || localPlayer->itemId == 276 || localPlayer->itemId == 283 /*swords*/ || localPlayer->itemId == 271 || localPlayer->itemId == 275 || localPlayer->itemId == 279 || localPlayer->itemId == 286 || localPlayer->itemId == 258 /*axes*/)) {
+				return;
+			}
+			if (click && !g_Data.isLeftClickDown()) return;
+			appl.x /= (100 - verticalspeed);
+			appl.y /= (100 - horizontalspeed);
+			if (appl.x >= 1 || appl.x <= -1) appl.div(abs(appl.x));
+			if (appl.y >= 1 || appl.y <= -1) appl.div(abs(appl.y));
+			if(!vertical)
+			appl.x = 0;
 		localPlayer->applyTurnDelta(&appl);
+	}
+		lastTarget = targetList[0];
 	}
 	
 }
-
-
