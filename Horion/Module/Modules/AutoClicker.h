97c5958149c967d7944821ca53cfc24ea9f60137
zzolr <49467061+zzolr@users.noreply.github.com>
2019-08-25 12:30:18 -0700

    Create AutoClicker.h (#46)

---
+++ Horion/Module/Modules/AutoClicker.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Module.h"
+
+class AutoClicker : public IModule
+{
+private:
+	int delay = 0;
+	int Odelay = 0;
+	bool sword = true;
+	//bool debug = false;
+public:
+	AutoClicker();
+	~AutoClicker();
+
+	// Inherited via IModule
+	virtual const char* getModuleName() override;
+	virtual void onTick(C_GameMode* gm) override;
+};
+
