5ce91dc40577fb4a93452311602f4712941a887e
Qther <dtaclanlegion@gmail.com>
2019-08-26 21:05:03 +0800

    AutoTotem now sets your offhand to Totem
    I don't know how to make settings so don't bother asking me to make them thanks.

--- Horion/Module/Modules/AutoTotem.cpp
+++ Horion/Module/Modules/AutoTotem.cpp
@@ -1,5 +1,5 @@
 #include "AutoTotem.h"
-
+#include "../../Command/Commands/setoffhandCommand.h"
 
 
 AutoTotem::AutoTotem() : IModule(0x0, PLAYER)
@@ -20,23 +20,21 @@ void AutoTotem::onTick(C_GameMode* gm) {
 	
 	if (g_Data.getLocalPlayer() != nullptr) 
 	{
-		C_ItemStack* i = g_Data.getLocalPlayer()->getEquippedTotem();
-
-		if (i->item == NULL && delay > 3) 
+		
+		if (delay > 3) 
 		{
-			C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
-			C_Inventory* a = supplies->inventory;
-			for (int i = 0; i < 36; i++) {
-				C_ItemStack* test = a->getItemStack(i);
-				if (test->item != NULL) {
-					C_Item* yikes = *test->item;
-					if (yikes->itemId == 450) {
-						g_Data.getLocalPlayer()->consumeTotem();
-						g_Data.getLocalPlayer()->setOffhandSlot(test);
-					}
+			int itemId = 450;
+			char count = 1;
+			char itemData = 0;
 
-				}
-			}
+			using getItemFromId_t = C_ItemStack * (__fastcall*)(void*, int itemID);
+			static getItemFromId_t  getItemFromId = reinterpret_cast<getItemFromId_t>(Utils::FindSignature("40 53 48 83 EC 30 48 8B D9 66 85 D2 0F 84 ?? ?? ?? ?? 44 0F BF CA 48 8D 4C 24 ?? 44 89 4C 24 ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? 66 0F 1F 44 00 00 0F B6 01 48 8D 49 ??"));
+
+			void* ItemPtr = malloc(0x8);
+			C_ItemStack* cStack = getItemFromId(ItemPtr, itemId);
+			C_ItemStack* yot = reinterpret_cast<C_ItemStack*>(malloc(0x88));
+			yot->ItemStackConstructor(*cStack->item, count, itemData);
+			g_Data.getLocalPlayer()->setOffhandSlot(yot);
 			delay = 0;
 		}
 
