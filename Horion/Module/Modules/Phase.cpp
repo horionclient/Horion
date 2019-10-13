92d70a13766f847cc067f898aca2f7fa671a8779
Greloom-Yeet <56508468+Greloom-Yeet@users.noreply.github.com>
2019-10-14 00:50:44 +1000

    if you jump you go up, if you crouch you go down

--- Horion/Module/Modules/Phase.cpp
+++ Horion/Module/Modules/Phase.cpp
@@ -17,8 +17,16 @@ const char* Phase::getModuleName()
 }
 
 void Phase::onTick(C_GameMode* gm) {
-	if(gm->player != nullptr)
-		gm->player->aabb.upper.y = gm->player->aabb.lower.y;
+    if(gm->player != nullptr)
+        gm->player->aabb.upper.y = gm->player->aabb.lower.y;
+
+    if (gm->player->isSneaking) {
+        gm->player->setPos(gm->player->getPos - vec3_t(0, 1, 0));
+    }
+
+    if (gm->player->isJumping) {
+        gm->player->setPos(gm->player->getPos + vec3_t(0, 1, 0));
+    }
 }
 void Phase::onDisable() {
 	if (g_Data.getLocalPlayer() != nullptr)
