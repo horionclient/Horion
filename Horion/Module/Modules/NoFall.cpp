6c28206bc83686f07c49f56d7f38fd83f27d65ec
Greloom-Yeet <56508468+Greloom-Yeet@users.noreply.github.com>
2019-10-14 00:25:44 +1000

    literally just set fall distance to 0.5
    fixing elytra

--- Horion/Module/Modules/NoFall.cpp
+++ Horion/Module/Modules/NoFall.cpp
@@ -18,7 +18,7 @@ const char* NoFall::getModuleName()
 
 void NoFall::onTick(C_GameMode* gm) {
 	if (gm->player != nullptr) {
-		gm->player->fallDistance = 0;
+		gm->player->fallDistance = 0.5;
 	}
 }
 
