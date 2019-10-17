ae362a0ff4fac980efa14715200d016e543521ed
Damakermiddle <39424163+Damakermiddle@users.noreply.github.com>
2019-10-17 10:40:50 -0400

    Update StackableItem.cpp
    fix yikes

--- Horion/Module/Modules/StackableItem.cpp
+++ Horion/Module/Modules/StackableItem.cpp
@@ -25,9 +25,9 @@ void StackableItem::onTick(C_GameMode* gm) {
 		for (int i = 0; i < 36; i++) {
 			C_ItemStack* test = a->getItemStack(i);
 			if (test->item != NULL) {
-				C_Item* yikes = *test->item;
-				yikes->setStackedByData(true);
-				yikes->setMaxStackSize(64);
+				C_Item* x = *test->item;
+				x->setStackedByData(true);
+				x->setMaxStackSize(64);
 			}
 		}
 	}
