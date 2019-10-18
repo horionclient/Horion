214bc2eb0d9bf55a9db8be8b9c4ad69cec748709
Richard <mail@richard.yt>
2019-10-18 19:55:16 +0200

    Added ChestAura

---
+++ Horion/Module/Modules/ChestAura.cpp
@@ -0,0 +1,44 @@
+#include "ChestAura.h"
+#include "ChestAura.h"
+
+ChestAura::ChestAura() : IModule(0x0, PLAYER, "Aura for opening chests")
+{
+}
+
+
+ChestAura::~ChestAura()
+{
+}
+
+const char* ChestAura::getModuleName()
+{
+	return ("ChestAura");
+}
+
+void ChestAura::onTick(C_GameMode* gm) {
+	if (gm->player == nullptr) return;
+	if (g_Data.getLocalPlayer()->getSupplies()->inventory->isFull()) return;
+	vec3_t* pos = gm->player->getPos();
+	for (int x = std::abs(pos->x) - 3; x < std::abs(pos->x) + 3; x++) {
+	for (int z = std::abs(pos->z) - 3; z < std::abs(pos->z) + 3; z++) {
+	for (int y = std::abs(pos->y) - 3; y < std::abs(pos->y) + 3; y++) {
+		vec3_ti pos = vec3_ti(x, y, z);
+		C_Block* block = gm->player->region->getBlock(pos);
+		if (block != nullptr && g_Data.canUseMoveKeys()) {
+			if (block->toLegacy()->blockId == 54) {
+				if (!(std::find(chestlist.begin(), chestlist.end(), pos) != chestlist.end())) {
+					g_Data.getCGameMode()->buildBlock(&pos, 0);
+					chestlist.push_back(pos);
+				}
+			}
+		}
+
+	}
+	}
+	}
+}
+
+void ChestAura::onDisable()
+{
+	this->chestlist.clear(); // this code should be changed later, the chestlist has to be cleared when loading into a new world
+}
