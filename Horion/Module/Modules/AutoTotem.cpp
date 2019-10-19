6c49a71e3eb7d3513610571c5fa370fa3647bbd1
NewGrelAltNewLife <56692309+NewGrelAltNewLife@users.noreply.github.com>
2019-10-19 13:44:42 +1000

    Update AutoTotem.cpp

--- Horion/Module/Modules/AutoTotem.cpp
+++ Horion/Module/Modules/AutoTotem.cpp
@@ -13,35 +13,34 @@ AutoTotem::~AutoTotem()
 
 const char* AutoTotem::getModuleName()
 {
-	return ("AutoTotem");
+    return ("AutoTotem");
 }
 
 void AutoTotem::onTick(C_GameMode* gm) {
-	
-	if (g_Data.getLocalPlayer() != nullptr) 
-	{
-		C_ItemStack* offhandTotem = g_Data.getLocalPlayer()->getEquippedTotem();
+    
+    if (g_Data.getLocalPlayer() != nullptr) 
+    {
+        C_ItemStack* offhandTotem = g_Data.getLocalPlayer()->getEquippedTotem();
 
-		if (offhandTotem->item == NULL && delay > 3) 
-		{
-			C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
-			C_Inventory* a = supplies->inventory;
-			for (int offhandTotem = 0; offhandTotem < 36; i++) {
-				C_ItemStack* test = a->getItemStack(i);
-				if (test->item != NULL) {
-					C_Item* totem = *test->item;
-					if (totem->itemId == 450) {
-						g_Data.getLocalPlayer()->consumeTotem();
-						g_Data.getLocalPlayer()->setOffhandSlot(test);
-					}
+        if (offHandTotem->item == NULL && delay > 3) 
+        {
+            C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
+            C_Inventory* inventory = supplies->inventory;
+            for (int slot = 0; slot < 36; slot++) {
+                C_ItemStack* slotItem = slot->getItemStack(i);
+                if (slotItem->item != NULL) {
+                    C_Item* slotItemId = *slotItem->item;
+                    if (slotItemId->item == 450) {
+                        g_Data.getLocalPlayer()->consumeTotem();
+                        g_Data.getLocalPlayer()->setOffhandSlot(test);
+                    }
+                }
+            }
+            delay = 0;
+        }
 
-				}
-			}
-			delay = 0;
-		}
-
-		delay++;
-	}
+        delay++;
+    }
 }
 
 
