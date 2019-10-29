225d962c10c32fac8fe441067bb6ca65d42971d2
Monte Hellawell <monte@montudor.com>
2019-10-29 13:18:10 +0000

    Update TeleportCommand.cpp

--- Horion/Command/Commands/TeleportCommand.cpp
+++ Horion/Command/Commands/TeleportCommand.cpp
@@ -20,9 +20,9 @@ bool TeleportCommand::execute(std::vector<std::string>* args)
 	assertTrue(args->size() >= 4);
 
 	vec3_t pos;
-	pos.x = assertFloat(args->at(1))+(float)0.5;
+	pos.x = assertFloat(args->at(1))+0.5f;
 	pos.y = assertFloat(args->at(2))+(float)g_Data.getLocalPlayer()->height;
-	pos.z = assertFloat(args->at(3))+(float)0.5;
+	pos.z = assertFloat(args->at(3))+0.5f;
 
 	g_Data.getLocalPlayer()->setPos(pos);
 	clientMessageF("%sTeleported!", GREEN);
