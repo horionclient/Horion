663a0364a5214925a1e092b388b9318164211ec8
erwan celanie <ti-yoyo789@hotmail.fr>
2019-12-14 23:14:22 -0400

    fix crash

--- Horion/Module/Modules/Criticals.cpp
+++ Horion/Module/Modules/Criticals.cpp
@@ -16,7 +16,7 @@ const char* Criticals::getModuleName()
 
 void Criticals::onSendPacket(C_Packet* packet)
 {
-	if (packet->isInstanceOf<C_MovePlayerPacket>() && !g_Data.getLocalPlayer()->onGround)
+	if (packet->isInstanceOf<C_MovePlayerPacket>() && g_Data.getLocalPlayer() != nullptr && !g_Data.getLocalPlayer()->onGround)
 	{
 		C_MovePlayerPacket* movePacket = reinterpret_cast<C_MovePlayerPacket*>(packet);
 		movePacket->onGround = false;
