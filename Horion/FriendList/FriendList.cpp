3df10dab8011b5224e89116fbfe7934a06119ca6
OhBoyItsGrelAgain <cawebod928@hide-mail.net>
2019-12-23 09:36:07 +1000

    FriendList

--- Horion/FriendList/FriendList.cpp
+++ Horion/FriendList/FriendList.cpp
@@ -2,18 +2,15 @@
 
 FriendList g_friend;
 
-FriendList::FriendList()
-{
+FriendList::FriendList() {
 	
 }
 
-FriendList::~FriendList()
-{
+FriendList::~FriendList() {
 
 }
 
-void FriendList::addPlayerToList(std::string Name)
-{
+void FriendList::addPlayerToList(std::string Name) {
 	g_friend.List.push_back(Name);
 }
 
@@ -21,29 +18,23 @@ std::vector<std::string> FriendList::getList() {
 	return g_friend.List;
 }
 
-bool FriendList::findPlayer(std::string name)
-{
+bool FriendList::findPlayer(std::string name) {
 	//std::transform(name.begin(), name.end(), name.begin(), ::tolower);
-	for (std::vector<std::string>::iterator it = g_friend.List.begin(); it != g_friend.List.end(); ++it)
-	{
+	for (std::vector<std::string>::iterator it = g_friend.List.begin(); it != g_friend.List.end(); ++it) {
 		//std::string copy = *it;
 		//std::transform(copy.begin(), copy.end(), copy.begin(), ::tolower);
-		if (it->find(name) != std::string::npos)
-		{
+		if (it->find(name) != std::string::npos) {
 			return true;
 		}
 	}
 	return false;
 }
 
-bool FriendList::removePlayer(std::string& g)
-{
-	for (std::vector<std::string>::iterator it = g_friend.List.begin(); it != g_friend.List.end(); ++it)
-	{
+bool FriendList::removePlayer(std::string& g) {
+	for (std::vector<std::string>::iterator it = g_friend.List.begin(); it != g_friend.List.end(); ++it) {
 		std::string copy = *it;
 		std::transform(copy.begin(), copy.end(), copy.begin(), ::tolower);
-		if (copy.find(g) != std::string::npos)
-		{
+		if (copy.find(g) != std::string::npos) {
 			g = *it;
 			g_friend.List.erase(it);
 			return true;
