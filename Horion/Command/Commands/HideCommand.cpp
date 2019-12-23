1287e2552be5b04ddbd29300ba3d2d729733f62d
StopFlaggingMeJesusChrist <58966590+StopFlaggingMeJesusChrist@users.noreply.github.com>
2019-12-23 20:17:40 +1000

    woah richard that's awesome 😳

--- Horion/Command/Commands/HideCommand.cpp
+++ Horion/Command/Commands/HideCommand.cpp
@@ -2,7 +2,7 @@
 
 
 
-HideCommand::HideCommand() : IMCCommand("hide", "Hide Horion Utility Mod.", "") {
+HideCommand::HideCommand() : IMCCommand("hide", "Hides Horion.", "") {
 }
 
 
@@ -12,10 +12,10 @@ HideCommand::~HideCommand() {
 bool HideCommand::execute(std::vector<std::string>* args) {
 	GameData::hide();
 	if (GameData::shouldHide()) {
-		clientMessageF("[%sHorion%s] %sThe Horion Utility Mod has been hidden.", GOLD, WHITE, GREEN);
+		clientMessageF("[%sHorion%s] %sHorion has been hidden.", GOLD, WHITE, GREEN);
 	}
 	else {
-		clientMessageF("[%sHorion%s] %sHorion Utility Mod is now visible.", GOLD, WHITE, GREEN);
+		clientMessageF("[%sHorion%s] %sHorion is now visible.", GOLD, WHITE, GREEN);
 	}
 	return true;
 }
