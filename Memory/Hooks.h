d9ed32e7ff76c6f8ea507b3fc43950bdf9ccd98a
GodGamer029 <godgamer029@gmail.com>
2019-12-27 21:03:23 +0100

    Changes to clang-format

--- Memory/Hooks.h
+++ Memory/Hooks.h
@@ -4,24 +4,24 @@
 //#define PERFORMANCE_TEST
 #endif
 
-#include "MinHook.h"
-#include "../SDK/CGameMode.h"
-#include "../SDK/CChestBlockActor.h"
-#include "../SDK/CBlockLegacy.h"
-#include "GameData.h"
-#include "../Horion/Config/ConfigManager.h"
-#include "../SDK/TextHolder.h"
-#include "../SDK/CMinecraftUIRenderContext.h"
-#include "../Horion/Module/ModuleManager.h"
-#include "../Horion/DrawUtils.h"
 #include "../Horion/Command/CommandMgr.h"
-#include "../Utils/TextFormat.h"
-#include "../Horion/Menu/TabGui.h"
+#include "../Horion/Config/ConfigManager.h"
+#include "../Horion/DrawUtils.h"
 #include "../Horion/Menu/ClickGui.h"
-#include "../SDK/CUIScene.h"
+#include "../Horion/Menu/TabGui.h"
+#include "../Horion/Module/ModuleManager.h"
+#include "../SDK/CBlockLegacy.h"
+#include "../SDK/CChestBlockActor.h"
+#include "../SDK/CGameMode.h"
+#include "../SDK/CMinecraftUIRenderContext.h"
 #include "../SDK/CMoveInputHandler.h"
 #include "../SDK/CRakNetInstance.h"
+#include "../SDK/CUIScene.h"
+#include "../SDK/TextHolder.h"
+#include "../Utils/TextFormat.h"
 #include "../resource.h"
+#include "GameData.h"
+#include "MinHook.h"
 
 #ifdef PERFORMANCE_TEST
 #include <chrono>
@@ -29,15 +29,14 @@
 
 #include <dxgi.h>
 
-
 struct SkinData {
 public:
-	char pad_0x0000[0x4]; //0x0000
-	unsigned int SkinWidth; //0x0004 
-	unsigned int SkinHeight; //0x0008 
-	char pad_0x000C[0x4]; //0x000C
-	void* skinData; //0x0010 
-	size_t skinSize; //0x0018 
+	char pad_0x0000[0x4];     //0x0000
+	unsigned int SkinWidth;   //0x0004
+	unsigned int SkinHeight;  //0x0008
+	char pad_0x000C[0x4];     //0x000C
+	void* skinData;           //0x0010
+	size_t skinSize;          //0x0018
 };
 
 class VMTHook;
@@ -46,46 +45,46 @@ class FuncHook;
 class Hooks {
 private:
 	bool shouldRender = true;
+
 public:
-	
 	static void Init();
 	static void Restore();
 
 private:
-	static void		GameMode_tick(C_GameMode* _this);
-	static void		SurvivalMode_tick(C_GameMode* _this);
-	static void		ChatScreenController_sendChatMessage(uint8_t* _this);
-	static __int64	UIScene_setupAndRender(C_UIScene* uiscene, __int64 screencontext);
-	static __int64	UIScene_render(C_UIScene* uiscene, __int64 screencontext);
-	static __int64	RenderText(__int64 a1, C_MinecraftUIRenderContext* renderCtx);
-	static float*	Dimension_getFogColor(__int64, float* color, float brightness);
-	static float	Dimension_getTimeOfDay(__int64, int a2, float a3);
-	static float	Dimension_getSunIntensity(__int64, float a2, vec3_t* a3, float a4);
-	static void		ChestBlockActor_tick(C_ChestBlockActor*, void* a);
-	static void		Actor_lerpMotion(C_Entity* _this, vec3_t);
-	static int		AppPlatform_getGameEdition(__int64 _this);
-	static void		PleaseAutoComplete(__int64 _this, __int64 a2, TextHolder* text, int a4);
-	static void		LoopbackPacketSender_sendToServer(C_LoopbackPacketSender* a, C_Packet* packet);
-	static float	LevelRendererPlayer_getFov(__int64 _this, float a2, bool a3);
-	static void		MultiLevelPlayer_tick(C_EntityList* entityList);
-	static void		GameMode_startDestroyBlock(C_GameMode* _this, vec3_ti* a2, uint8_t face, void* a4, void* a5);
-	static void		HIDController_keyMouse(C_HIDController* _this, void* a2, void* a3);
-	static int		BlockLegacy_getRenderLayer(C_BlockLegacy* a1);
-	static __int8*	BlockLegacy_getLightEmission(C_BlockLegacy* _this, __int8* a2);
-	static __int64	LevelRenderer_renderLevel(__int64 _this, __int64 a2, __int64 a3);
-	static void		ClickFunc(__int64 a1, char a2, char a3, __int16 a4, __int16 a5, __int16 a6, __int16 a7, char a8);
-	static __int64	MoveInputHandler_tick(C_MoveInputHandler* _this, C_Entity* a2);
-	static __int64	ChestScreenController_tick(C_ChestScreenController* _this);
-	static __int64	GetGamma(__int64 a1);
-	static bool		Actor_isInWater(C_Entity* _this);
-	static void		JumpPower(C_Entity* _this, float a2);
-	static __int64	MinecraftGame_onAppSuspended(__int64 _this);
-	static void		Actor_ladderUp(C_Entity* _this);
-	static void		Actor_startSwimming(C_Entity* _this);
-	static void		RakNetInstance_tick(C_RakNetInstance* _this,__int64 a2,__int64 a3);
-	static float	GameMode_getPickRange(C_GameMode* _this, __int64 a2, char a3);
-	static __int64	ConnectionRequest_create(__int64 _this, __int64 privateKeyManager, void* a3, TextHolder* selfSignedId, TextHolder* serverAddress, __int64 clientRandomId, TextHolder* skinId, SkinData* a8, __int64 capeData, __int64 animatedImageDataArr, TextHolder* skinResourcePatch, TextHolder* skinGeometryData, TextHolder* skinAnimationData, bool isPremiumSkin, bool isPersonaSkin, TextHolder* deviceId, int inputMode, int uiProfile, int guiScale, TextHolder* languageCode, bool sendEduModeParams, TextHolder* tenantId, __int64 unused, TextHolder* platformUserId, TextHolder* thirdPartyName, bool thirdPartyNameOnly, TextHolder* platformOnlineId, TextHolder* platformOfflineId, bool isCapeOnClassicSkin, TextHolder* capeId);
-	static void		InventoryTransactionManager_addAction(C_InventoryTransactionManager* a1, C_InventoryAction* a2);
+	static void GameMode_tick(C_GameMode* _this);
+	static void SurvivalMode_tick(C_GameMode* _this);
+	static void ChatScreenController_sendChatMessage(uint8_t* _this);
+	static __int64 UIScene_setupAndRender(C_UIScene* uiscene, __int64 screencontext);
+	static __int64 UIScene_render(C_UIScene* uiscene, __int64 screencontext);
+	static __int64 RenderText(__int64 a1, C_MinecraftUIRenderContext* renderCtx);
+	static float* Dimension_getFogColor(__int64, float* color, float brightness);
+	static float Dimension_getTimeOfDay(__int64, int a2, float a3);
+	static float Dimension_getSunIntensity(__int64, float a2, vec3_t* a3, float a4);
+	static void ChestBlockActor_tick(C_ChestBlockActor*, void* a);
+	static void Actor_lerpMotion(C_Entity* _this, vec3_t);
+	static int AppPlatform_getGameEdition(__int64 _this);
+	static void PleaseAutoComplete(__int64 _this, __int64 a2, TextHolder* text, int a4);
+	static void LoopbackPacketSender_sendToServer(C_LoopbackPacketSender* a, C_Packet* packet);
+	static float LevelRendererPlayer_getFov(__int64 _this, float a2, bool a3);
+	static void MultiLevelPlayer_tick(C_EntityList* entityList);
+	static void GameMode_startDestroyBlock(C_GameMode* _this, vec3_ti* a2, uint8_t face, void* a4, void* a5);
+	static void HIDController_keyMouse(C_HIDController* _this, void* a2, void* a3);
+	static int BlockLegacy_getRenderLayer(C_BlockLegacy* a1);
+	static __int8* BlockLegacy_getLightEmission(C_BlockLegacy* _this, __int8* a2);
+	static __int64 LevelRenderer_renderLevel(__int64 _this, __int64 a2, __int64 a3);
+	static void ClickFunc(__int64 a1, char a2, char a3, __int16 a4, __int16 a5, __int16 a6, __int16 a7, char a8);
+	static __int64 MoveInputHandler_tick(C_MoveInputHandler* _this, C_Entity* a2);
+	static __int64 ChestScreenController_tick(C_ChestScreenController* _this);
+	static __int64 GetGamma(__int64 a1);
+	static bool Actor_isInWater(C_Entity* _this);
+	static void JumpPower(C_Entity* _this, float a2);
+	static __int64 MinecraftGame_onAppSuspended(__int64 _this);
+	static void Actor_ladderUp(C_Entity* _this);
+	static void Actor_startSwimming(C_Entity* _this);
+	static void RakNetInstance_tick(C_RakNetInstance* _this, __int64 a2, __int64 a3);
+	static float GameMode_getPickRange(C_GameMode* _this, __int64 a2, char a3);
+	static __int64 ConnectionRequest_create(__int64 _this, __int64 privateKeyManager, void* a3, TextHolder* selfSignedId, TextHolder* serverAddress, __int64 clientRandomId, TextHolder* skinId, SkinData* a8, __int64 capeData, __int64 animatedImageDataArr, TextHolder* skinResourcePatch, TextHolder* skinGeometryData, TextHolder* skinAnimationData, bool isPremiumSkin, bool isPersonaSkin, TextHolder* deviceId, int inputMode, int uiProfile, int guiScale, TextHolder* languageCode, bool sendEduModeParams, TextHolder* tenantId, __int64 unused, TextHolder* platformUserId, TextHolder* thirdPartyName, bool thirdPartyNameOnly, TextHolder* platformOnlineId, TextHolder* platformOfflineId, bool isCapeOnClassicSkin, TextHolder* capeId);
+	static void InventoryTransactionManager_addAction(C_InventoryTransactionManager* a1, C_InventoryAction* a2);
 
 	std::unique_ptr<FuncHook> GameMode_tickHook;
 	std::unique_ptr<FuncHook> SurvivalMode_tickHook;
@@ -125,19 +124,17 @@ private:
 
 extern Hooks g_Hooks;
 
-class FuncHook
-{
+class FuncHook {
 public:
-	void *funcPtr;
-	void *funcReal;
+	void* funcPtr;
+	void* funcReal;
 
 	FuncHook(void* func, void* hooked) {
 		funcPtr = func;
-		
+
 		MH_STATUS ret = MH_CreateHook(func, hooked, &funcReal);
 		if (ret == MH_OK && (__int64)func > 10) {
-			
-		}else
+		} else
 			logF("MH_CreateHook = %i", ret);
 	};
 
@@ -146,9 +143,7 @@ public:
 
 		MH_STATUS ret = MH_CreateHook(funcPtr, hooked, &funcReal);
 		if (ret == MH_OK && (__int64)funcPtr > 10) {
-
-		}
-		else
+		} else
 			logF("MH_CreateHook = %i", ret);
 	};
 
@@ -163,13 +158,12 @@ public:
 	}
 
 	void Restore() {
-		if(this != nullptr && funcPtr != nullptr)
+		if (this != nullptr && funcPtr != nullptr)
 			MH_DisableHook(funcPtr);
 	}
 
-	template<typename TRet, typename ... TArgs>
-	inline auto* GetFastcall()
-	{
+	template <typename TRet, typename... TArgs>
+	inline auto* GetFastcall() {
 		using Fn = TRet(__fastcall*)(TArgs...);
 		return reinterpret_cast<Fn>(funcReal);
 	};
