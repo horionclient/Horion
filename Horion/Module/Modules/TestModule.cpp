a359326f079bb420e3da906efad9c5673d2bee03
erwan celanie <ti-yoyo789@hotmail.fr>
2020-02-05 12:15:49 -0400

    added not really working drawImage function

--- Horion/Module/Modules/TestModule.cpp
+++ Horion/Module/Modules/TestModule.cpp
@@ -1,4 +1,5 @@
 #include "TestModule.h"
+#include "../../../resource.h"
 
 TestModule::TestModule() : IModule(0, Category::EXPLOITS, "For testing purposes") {
 }
@@ -14,6 +15,12 @@ void TestModule::onTick(C_GameMode* gm) {
 }
 
 void TestModule::onPostRender(C_MinecraftUIRenderContext* renderCtx) {
+	vec2_t pos(200.5, 23.5);
+	float imageHeight = 100.f;
+	float imageWidth = 120.f;
+	vec2_t idk(80.f, 100.f);
+	std::string filePath = "textures/entity/fireworks";
+	DrawUtils::drawImage(filePath, pos, vec2_t(imageWidth, imageHeight), idk);
 }
 
 void TestModule::onDisable() {
