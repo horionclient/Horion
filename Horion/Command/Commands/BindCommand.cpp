48b1481526ac2669af25393a09298e2b9a4696d0
GodGamer029 <godgamer029@gmail.com>
2020-02-23 21:32:06 +0100

    Bind command improvements

--- Horion/Command/Commands/BindCommand.cpp
+++ Horion/Command/Commands/BindCommand.cpp
@@ -10,62 +10,69 @@ BindCommand::~BindCommand() {
 }
 
 bool BindCommand::execute(std::vector<std::string>* args) {
-	assertTrue(args->size() >= 3);
+	assertTrue(args->size() >= 2);
 	std::string moduleName = args->at(1);
-	std::string key = args->at(2);
-
 	assertTrue(moduleName.size() > 0);
-	assertTrue(key.size() > 0);
 
-	if (key.size() > 1) {
-		std::transform(key.begin(), key.end(), key.begin(), ::tolower);
-		const char* needle = key.c_str();
-
-		IModule* mod = moduleMgr->getModuleByName(moduleName);
-
-		if (key == "none") {
-			mod->setKeybind(0x0);
-			clientMessageF("%sSuccessfully unbound %s!", GREEN, mod->getModuleName());
-			return true;
-		}
-
-		for (int i = 0; i < 190; i++) {
-			const char* haystack = KeyNames[i];
-			size_t len = strlen(needle) + 1;
-			char* haystackLowercase = new char[len];
-			for (int i = 0; i < len; i++)
-				haystackLowercase[i] = tolower(haystack[i]);
-
-			if (strcmp(needle, haystackLowercase) == 0) {
-				if (mod == nullptr) {
-					clientMessageF("%sCould not find module with name: %s", RED, moduleName.c_str());
-				} else {
-					mod->setKeybind(i);
-					clientMessageF("%sThe keybind of %s is now '%s'", GREEN, mod->getModuleName(), haystack);
-				}
-				delete[] haystackLowercase;
-				return true;
-			}
-			delete[] haystackLowercase;
-		}
-		clientMessageF("%sInvalid key!", RED);
+	IModule* mod = moduleMgr->getModuleByName(moduleName);
+	if (mod == nullptr) {
+		clientMessageF("%sCould not find module with name: %s", RED, moduleName.c_str());
 		return true;
 	}
 
-	int keyCode = (int)key.at(0);
-	if (keyCode >= 0x61 && keyCode <= 0x7A)  // Convert key to lower case
-		keyCode -= 0x20;
+	if (args->size() >= 3) {
+		std::string key = args->at(2);
 
-	if (keyCode >= 0x30 && keyCode <= 0x5A) {
-		IModule* mod = moduleMgr->getModuleByName(moduleName);
-		if (mod == nullptr) {
-			clientMessageF("%sCould not find module with name: %s", RED, moduleName.c_str());
-		} else {
-			mod->setKeybind(keyCode);
-			clientMessageF("%sThe Keybind of %s is now '%c'", GREEN, mod->getModuleName(), keyCode);
+		assertTrue(key.size() > 0);
+
+		if (key.size() > 1) {
+			std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+			const char* needle = key.c_str();
+
+			if (key == "none") {
+				mod->setKeybind(0x0);
+				clientMessageF("%sSuccessfully unbound %s!", GREEN, mod->getModuleName());
+				return true;
+			}
+
+			for (int i = 0; i < 190; i++) {
+				const char* haystack = KeyNames[i];
+				size_t len = strlen(needle) + 1;
+				char* haystackLowercase = new char[len];
+				for (int i = 0; i < len; i++)
+					haystackLowercase[i] = tolower(haystack[i]);
+
+				if (strcmp(needle, haystackLowercase) == 0) {
+					mod->setKeybind(i);
+					clientMessageF("%sThe keybind of %s is now '%s'", GREEN, mod->getModuleName(), haystack);
+					delete[] haystackLowercase;
+					return true;
+				}
+				delete[] haystackLowercase;
+			}
+			clientMessageF("%sInvalid key!", RED);
+			return true;
 		}
+
+		int keyCode = (int)key.at(0);
+		if (keyCode >= 0x61 && keyCode <= 0x7A)  // Convert key to lower case
+			keyCode -= 0x20;
+
+		if (keyCode >= 0x30 && keyCode <= 0x5A) {
+			IModule* mod = moduleMgr->getModuleByName(moduleName);
+			if (mod == nullptr) {
+				clientMessageF("%sCould not find module with name: %s", RED, moduleName.c_str());
+			} else {
+				mod->setKeybind(keyCode);
+				clientMessageF("%sThe Keybind of %s is now '%c'", GREEN, mod->getModuleName(), keyCode);
+			}
+		} else {
+			clientMessageF("%sInvalid Key! Outside of ascii range: %X", RED, keyCode);
+		}
+		return true;
 	} else {
-		clientMessageF("%sInvalid Key! Outside of ascii range: %X", RED, keyCode);
+		clientMessageF("%s%s is currently bound to %s", RED, mod->getModuleName(), Utils::getKeybindName(mod->getKeybind()));
+		return true;
 	}
-	return true;
+	
 }
