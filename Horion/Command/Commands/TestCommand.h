698c6ab91913b89220ea915c8791e488a617620e
godgamer029 <godgamer029@gmail.com>
2020-03-26 16:37:54 +0100

    Right

--- Horion/Command/Commands/TestCommand.h
+++ Horion/Command/Commands/TestCommand.h
@@ -4,12 +4,7 @@
 #include "../../../Utils/SkinUtil.h"
 #include "../../../resource.h"
 #include <thread>
-
-//#define HTTP_TEST
-
-#ifdef HTTP_TEST
-#include "../../../include/WinHttpClient.h"
-#endif
+#include "../../../Utils/ChakraHelper.h"
 
 class TestCommand : public IMCCommand {
 public:
