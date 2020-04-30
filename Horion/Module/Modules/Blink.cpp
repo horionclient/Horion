bc438e90f1aa115459d35e06ab1152df4c7c0c51
Richard <mail@richard.yt>
2020-04-30 14:29:55 +0200

    fix blink name

--- Horion/Module/Modules/Blink.cpp
+++ Horion/Module/Modules/Blink.cpp
@@ -12,10 +12,6 @@ const char* Blink::getModuleName() {
 	return ("Blink");
 }
 
-const char* Blink::getRawModuleName() {
-	return ("Blink_");
-}
-
 void Blink::onTick(C_GameMode* gm) {
 	gm->player->fallDistance = 0.f;
 }
