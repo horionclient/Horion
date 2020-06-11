90337b172f0074498c91a503786908e8d6a1bf22
Richard <mail@richard.yt>
2020-06-12 00:31:54 +0200

    escape codes in .say

--- Horion/Command/Commands/NameSpoofCommand.cpp
+++ Horion/Command/Commands/NameSpoofCommand.cpp
@@ -8,30 +8,6 @@ NameSpoofCommand::NameSpoofCommand() : IMCCommand("namespoof", "Spoof your name 
 NameSpoofCommand::~NameSpoofCommand() {
 }
 
-std::string string_replace(const std::string& s, const std::string& findS, const std::string& replaceS) {
-	std::string result = s;
-	auto pos = s.find(findS);
-	if (pos == std::string::npos) {
-		return result;
-	}
-	result.replace(pos, findS.length(), replaceS);
-	return string_replace(result, findS, replaceS);
-}
-
-std::string parse(const std::string& s) {
-	static std::vector<std::pair<std::string, std::string> > patterns = {
-		{"\\\\", "\\"},
-		{"\\n", "\n"},
-		{"\\r", "\r"},
-		{"\\t", "\t"},
-		{"\\\"", "\""}};
-	std::string result = s;
-	for (const auto& p : patterns) {
-		result = string_replace(result, p.first, p.second);
-	}
-	return result;
-}
-
 bool NameSpoofCommand::execute(std::vector<std::string>* args) {
 	if (args->at(1) == "name" && args->size() > 2) {
 		std::ostringstream os;
@@ -40,7 +16,7 @@ bool NameSpoofCommand::execute(std::vector<std::string>* args) {
 				os << " ";
 			os << args->at(i);
 		}
-		TextHolder* name = new TextHolder(parse(os.str()));
+		TextHolder* name = new TextHolder(os.str());
 		g_Data.setFakeName(name);
 		clientMessageF("[%sHorion%s] %sSet fakename to %s%s%s, please reconnect!", GOLD, WHITE, GREEN, GRAY, name->getText(), GREEN);
 		return true;
--- Horion/Command/Commands/SayCommand.cpp
+++ Horion/Command/Commands/SayCommand.cpp
@@ -6,6 +6,31 @@ SayCommand::SayCommand() : IMCCommand("say", "Sends a chat message", "<message>"
 SayCommand::~SayCommand() {
 }
 
+std::string string_replace(const std::string& s, const std::string& findS, const std::string& replaceS) {
+	std::string result = s;
+	auto pos = s.find(findS);
+	if (pos == std::string::npos) {
+		return result;
+	}
+	result.replace(pos, findS.length(), replaceS);
+	return string_replace(result, findS, replaceS);
+}
+
+std::string parse(const std::string& s) {
+	static std::vector<std::pair<std::string, std::string> > patterns = {
+		{"\\\\", "\\"},
+		{"\\n", "\n"},
+		{"\\r", "\r"},
+		{"\\t", "\t"},
+		{"\\f", "\f"},
+		{"\\\"", "\""}};
+	std::string result = s;
+	for (const auto& p : patterns) {
+		result = string_replace(result, p.first, p.second);
+	}
+	return result;
+}
+
 bool SayCommand::execute(std::vector<std::string>* args) {
 	assertTrue(args->size() > 1);
 
@@ -16,7 +41,7 @@ bool SayCommand::execute(std::vector<std::string>* args) {
 		os << args->at(i);
 	}
 	C_TextPacket* textPacket = new C_TextPacket();
-	textPacket->message.setText(os.str());
+	textPacket->message.setText(parse(os.str()));
 	textPacket->sourceName.setText(g_Data.getLocalPlayer()->getNameTag()->getText());
 	textPacket->xboxUserId = std::to_string(g_Data.getLocalPlayer()->getUserId());
 	g_Data.getClientInstance()->loopbackPacketSender->sendToServer(textPacket);
