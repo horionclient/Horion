6e39b8ea8ae09f87e3ae440e9134e0ebd6a69584
erwan celanie <ti-yoyo789@hotmail.fr>
2020-06-26 21:43:04 -0400

    bruh

--- Memory/GameData.h
+++ Memory/GameData.h
@@ -213,17 +213,17 @@ public:
 	inline C_ClientInstance* getClientInstance() { return clientInstance; };
 	inline C_GuiData* getGuiData() { return clientInstance->getGuiData(); };
 	inline C_LocalPlayer* getLocalPlayer() {
-		/*#ifdef _BETA
+		#ifdef _BETA
 		unsigned int converted = networkedData.localPlayerOffset ^ networkedData.xorKey;
 		if (networkedData.localPlayerOffset < 0x110 || converted < 0x125 || converted > 0x191 || networkedData.dataSet == false)
 			localPlayer = nullptr;
 		else
-			localPlayer = *reinterpret_cast<C_LocalPlayer**>(reinterpret_cast<__int64>(clientInstance) + converted);*/
+			localPlayer = *reinterpret_cast<C_LocalPlayer**>(reinterpret_cast<__int64>(clientInstance) + converted);
 		
-		//#else
+		#else
 		localPlayer = clientInstance->localPlayer;
 		
-		//#endif
+		#endif
 		
 		if (localPlayer == nullptr)
 			gameMode = nullptr;
