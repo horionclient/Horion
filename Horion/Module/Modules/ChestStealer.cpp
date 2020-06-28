c9f21b141c097856ec7dfde60b6ae2c8378ad506
Richard <mail@richard.yt>
2020-06-28 13:06:42 +0200

    improve cheststealer

--- Horion/Module/Modules/ChestStealer.cpp
+++ Horion/Module/Modules/ChestStealer.cpp
@@ -1,7 +1,7 @@
 #include "ChestStealer.h"
 
 ChestStealer::ChestStealer() : IModule(0x0, Category::PLAYER, "Automatically takes all items out of a chest") {
-	registerIntSetting("Close Delay", &this->setDelay, this->setDelay, 3, 20);
+	registerIntSetting("Close Delay", &this->setDelay, this->setDelay, 0, 20);
 }
 
 ChestStealer::~ChestStealer() {
@@ -11,17 +11,28 @@ const char* ChestStealer::getModuleName() {
 	return ("ChestStealer");
 }
 
+std::vector<int> items;
+
 void ChestStealer::onTick(C_GameMode* gm) {
 	if (g_Data.getLocalPlayer()->canOpenContainerScreen() == 0 && chestScreenController != nullptr) {
-		for (int i = 0; i < 54; i++) {
-			chestScreenController->handleAutoPlace(0x7FFFFFFF, "container_items", i);
-		}
-		delay++;
-	}
-	if (g_Data.getLocalPlayer()->canOpenContainerScreen() == 0 && chestScreenController != nullptr && delay > setDelay) {
-		chestScreenController->leaveScreen();
-		delay = 0;
-	}
 
+		for (int i = 0; i < 54; i++) {
+			if (chestScreenController->_getItemStack(TextHolder("container_items"), i)->item != NULL)
+				items.push_back(i);
+		}
+
+		if (!items.empty()) {
+			for (int i : items) {
+				chestScreenController->handleAutoPlace(0x7FFFFFFF, "container_items", i);
+			}
+			items.clear();
+		} else  {
+			delay++;
+			if (delay > setDelay) {
+				chestScreenController->leaveScreen();
+				delay = 0;
+			}
+		}
+	}
 	chestScreenController = nullptr;
 }
