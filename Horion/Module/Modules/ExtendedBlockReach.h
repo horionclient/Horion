fa843bf63c826afd4797782d0a60eab05eb0f470
FloppyDolphin57 <50063641+FloppyDolphin57@users.noreply.github.com>
2020-09-05 11:30:36 -0400

    Add files via upload

---
+++ Horion/Module/Modules/ExtendedBlockReach.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Module.h"
+
+class ExtendedBlockReach : public IModule {
+private:
+	float blockReach = 50.f;
+
+public:
+	ExtendedBlockReach();
+	~ExtendedBlockReach();
+
+	inline float getBlockReach() { return blockReach; };
+	inline void setBlockReach(float reach) { blockReach = reach; };
+	// Inherited via IModule
+	virtual const char* getModuleName() override;
+};
