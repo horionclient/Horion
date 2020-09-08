4269ef5c72feec6ce7f0f31efbd1277cd4d15a39
Cryptxy <59030594+Cryptxy@users.noreply.github.com>
2020-09-08 04:24:02 +0100

    Add TimeChanger Module

---
+++ Horion/Module/Modules/TimeChanger.cpp
@@ -0,0 +1,18 @@
+#include "TimeChanger.h"
+
+TimeChanger::TimeChanger() : IModule(0x0, Category::VISUAL, "Changes the client-sided time.") {
+	registerFloatSetting("modifier", &this->modifier, this->modifier, 0.1f, 1.f);
+}
+
+TimeChanger::~TimeChanger() {
+}
+
+const char* TimeChanger::getModuleName() {
+	return "TimeChanger";
+}
+
+void TimeChanger::onEnable() {
+	static auto rainbowSkyMod = moduleMgr->getModule<RainbowSky>();
+	if (rainbowSkyMod->isEnabled())
+		rainbowSkyMod->setEnabled(false);
+}
---
+++ Horion/Module/Modules/TimeChanger.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../ModuleManager.h"
+#include "Module.h"
+
+class TimeChanger : public IModule {
+public:
+	TimeChanger();
+	~TimeChanger();
+
+	float modifier = 0.5f;
+
+	// Inherited via IModule
+	virtual const char* getModuleName() override;
+	virtual void onEnable() override;
+};
