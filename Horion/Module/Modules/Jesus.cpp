d6d7d00ec96af586a1cafdb83859ae3eb0226918
brampedgex <70824773+brampedgex@users.noreply.github.com>
2020-10-23 14:52:00 -0700

    Jesus now simulates water movement as if it's solid.
    I guess its possible to mess with the internal block data but I don't really want to do that because it could lead to any number of crashes

--- Horion/Module/Modules/Jesus.cpp
+++ Horion/Module/Modules/Jesus.cpp
@@ -1,6 +1,10 @@
 #include "Jesus.h"
 
-Jesus::Jesus() : IModule(0, Category::MOVEMENT, "Walk over water, like Jesus") {
+#include "../../../Utils/Logger.h"
+
+int jesusChrist = 0;
+
+Jesus::Jesus() : IModule(0, Category::MOVEMENT, "Allows you to walk on water and lava.") {
 }
 
 Jesus::~Jesus() {
@@ -12,20 +16,76 @@ const char* Jesus::getModuleName() {
 
 void Jesus::onTick(C_GameMode* gm) {
 	if (gm->player->isSneaking()) return;
+	if (gm->player->isOnFire()) return;
 
-	if (gm->player->hasEnteredWater()) {
-		gm->player->velocity.y = 0.06f;
+	vec3_t pos = *gm->player->getPos();
+	pos.y -= 1.62f;
+
+	pos.z = gm->player->aabb.upper.z;
+	pos.x = gm->player->aabb.upper.x; // upper upper
+
+	C_Block* block = g_Data.getLocalPlayer()->region->getBlock(vec3_ti(pos));
+	C_BlockLegacy* blockLegacy = *(block->blockLegacy);
+
+	if (blockLegacy->material->isLiquid && gm->player->velocity.y <= 0) {
+		vec3_t pos = *gm->player->getPos();
+		pos.y -= 0.62f;
+		pos.y = ceilf(pos.y);
+		pos.y += 0.61f;
+		gm->player->setPos(pos);
 		gm->player->onGround = true;
-		wasInWater = true;
-	} else if (gm->player->isInWater() || gm->player->isInLava()) {
+		gm->player->velocity.y = 0.f;
+	}
+
+	pos.x = gm->player->aabb.lower.x;
+	pos.z = gm->player->aabb.lower.z; // lower lower
+
+	block = g_Data.getLocalPlayer()->region->getBlock(vec3_ti(pos));
+	blockLegacy = *(block->blockLegacy);
+
+	if (blockLegacy->material->isLiquid && gm->player->velocity.y <= 0) {
+		vec3_t pos = *gm->player->getPos();
+		pos.y -= 0.62f;
+		pos.y = ceilf(pos.y);
+		pos.y += 0.61f;
+		gm->player->setPos(pos);
+		gm->player->onGround = true;
+		gm->player->velocity.y = 0.f;
+	}
+
+	pos.x = gm->player->aabb.upper.x; // upper x and lower z
+
+	block = g_Data.getLocalPlayer()->region->getBlock(vec3_ti(pos));
+	blockLegacy = *(block->blockLegacy);
+
+	if (blockLegacy->material->isLiquid && gm->player->velocity.y <= 0) {
+		vec3_t pos = *gm->player->getPos();
+		pos.y -= 0.62f;
+		pos.y = ceilf(pos.y);
+		pos.y += 0.61f;
+		gm->player->setPos(pos);
+		gm->player->onGround = true;
+		gm->player->velocity.y = 0.f;
+	}
+
+	pos.x = gm->player->aabb.lower.x;
+	pos.z = gm->player->aabb.upper.z; // lower x and upper z
+
+	block = g_Data.getLocalPlayer()->region->getBlock(vec3_ti(pos));
+	blockLegacy = *(block->blockLegacy);
+
+	if (blockLegacy->material->isLiquid && gm->player->velocity.y <= 0) {
+		vec3_t pos = *gm->player->getPos();
+		pos.y -= 0.62f;
+		pos.y = ceilf(pos.y);
+		pos.y += 0.61f;
+		gm->player->setPos(pos);
+		gm->player->onGround = true;
+		gm->player->velocity.y = 0.f;
+	}
+
+	if (gm->player->isInWater() || gm->player->isInLava()) {
 		gm->player->velocity.y = 0.1f;
 		gm->player->onGround = true;
-		wasInWater = true;
-	} else {
-		if (wasInWater) {
-			wasInWater = false;
-			gm->player->velocity.x *= 1.2f;
-			gm->player->velocity.x *= 1.2f;
-		}
 	}
 }
