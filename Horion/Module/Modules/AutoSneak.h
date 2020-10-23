c5e7fa9643566e15a565b20354e872637a1bfd9d
Operation-Ghost <48508820+Operation-Ghost@users.noreply.github.com>
2020-10-24 01:51:38 +1000

    added server side sneak to autoSneak

--- Horion/Module/Modules/AutoSneak.h
+++ Horion/Module/Modules/AutoSneak.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "Module.h"
 class AutoSneak : public IModule {
+private:
+	
+
 public:
+	bool doSilent = false;
 	AutoSneak();
 	~AutoSneak();
 
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
 	virtual void onDisable() override;
+	virtual void onSendPacket(C_Packet* packet) override;
+	virtual void onEnable() override;
 };
