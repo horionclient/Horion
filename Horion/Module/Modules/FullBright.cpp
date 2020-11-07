fb1104be2dd759f495ac8705b5512319abea23fb
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2020-11-07 16:02:42 +0100

    Fixed Crash

--- Horion/Module/Modules/FullBright.cpp
+++ Horion/Module/Modules/FullBright.cpp
@@ -10,21 +10,25 @@ const char* FullBright::getModuleName() {
 	return "Fullbright";
 }
 
-void FullBright::onTick(C_GameMode* gm) {
-	if (gammaPtr != nullptr && *gammaPtr != 10) 
-		*gammaPtr = 10;
-}
-
 void FullBright::onEnable() {
 	if (gammaPtr != nullptr) {
-		*prevGamma = *gammaPtr;
+		prevGamma = *gammaPtr;
+		*gammaPtr = 10;
+	}
+}
+
+void FullBright::onTick(C_GameMode* gm) {
+	if (gammaPtr != nullptr && *gammaPtr != 10) {
+		//if player joins world get original gamma (doesn't work)
+		if (prevGamma > 0.0f)
+			prevGamma = *gammaPtr;
 		*gammaPtr = 10;
 	}
 }
 
 void FullBright::onDisable() {
-	if (gammaPtr != nullptr || prevGamma != nullptr)
-		*gammaPtr = *prevGamma;
+	if (gammaPtr != nullptr && prevGamma > 0.0f)
+		*gammaPtr = prevGamma;
 	else if (gammaPtr != nullptr)
-		*gammaPtr = .5;
+		*gammaPtr = 1.0f;
 }
--- Horion/Module/Modules/FullBright.h
+++ Horion/Module/Modules/FullBright.h
@@ -3,7 +3,7 @@
 class FullBright : public IModule {
 public:
 	float* gammaPtr = nullptr;
-	float* prevGamma = nullptr;
+	float prevGamma = -1.0f;
 	FullBright();
 	~FullBright();
 
