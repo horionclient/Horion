a7d7824213faf94e5822c9dc03327b08b7bcf7dd
Mixu <hoppandebananen123@gmail.com>
2020-11-15 22:02:40 +0200

    Proper block object + function

--- Horion/Scripting/Functions/LevelFunctions.cpp
+++ Horion/Scripting/Functions/LevelFunctions.cpp
@@ -1,5 +1,28 @@
 #include "LevelFunctions.h"
 
+JsValueRef createBlock(C_BlockLegacy *block) {
+	JsValueRef ref;
+	chakra.JsCreateObject_(&ref);
+
+	std::wstring blockName = Utils::stringToWstring(block->name.getText());
+
+	JsValueRef bName;
+	chakra.JsPointerToString_(blockName.c_str(), blockName.size(), &bName);
+
+	std::wstring tileName = Utils::stringToWstring(block->tileName.getText());
+
+	JsValueRef tName;
+	chakra.JsPointerToString_(tileName.c_str(), tileName.size(), &tName);
+
+	JsValueRef id = chakra.toNumber(block->blockId);
+
+	chakra.addPropertyToObj(ref, L"name", bName);
+	chakra.addPropertyToObj(ref, L"tileName", tName);
+	chakra.addPropertyToObj(ref, L"blockId", id);
+
+	return ref;
+}
+
 JsValueRef CALLBACK LevelFunctions::isValid(JsValueRef callee, bool isConstructCall, JsValueRef* arguments, unsigned short argumentCount, void* callbackState) {
 	return chakra.toBoolean(g_Data.isInGame() && g_Data.getLocalPlayer()->isAlive());
 }
@@ -58,9 +81,5 @@ JsValueRef CALLBACK LevelFunctions::getBlock(JsValueRef callee, bool isConstruct
 	}
 
 	auto block = plr->region->getBlock(vecOpt.value())->toLegacy();
-	auto blockName = Utils::stringToWstring(block->name.getText());
-
-	JsValueRef ref;
-	chakra.JsPointerToString_(blockName.c_str(), blockName.size(), &ref);
-	return ref;
+	return createBlock(block);
 }
