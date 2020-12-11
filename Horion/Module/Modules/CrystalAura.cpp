4232e9d154bfc0176a2842c7a9090c460730660a
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2020-12-11 17:01:34 +0100

    Added Better CrystalAura

--- Horion/Module/Modules/CrystalAura.cpp
+++ Horion/Module/Modules/CrystalAura.cpp
@@ -1,56 +1,107 @@
 #include "CrystalAura.h"
 
 CrystalAura::CrystalAura() : IModule(VK_NUMPAD0, Category::COMBAT, "Destroys nearby Crystals") {
-	registerIntSetting("range", &range, range, 1, 10);
-	registerBoolSetting("autoplace", &autoplace, autoplace);
+	//registerIntSetting("range", &range, range, 1, 10);
+	registerIntSetting("Crystal range", &cRange, cRange, 1, 15);
+	registerIntSetting("Place range", &eRange, eRange, 1, 5);
+	registerIntSetting("Player range", &pRange, pRange, 1, 10);
+	registerBoolSetting("Autoplace", &autoplace, autoplace);
+	registerBoolSetting("Enhance place", &pEnhanced, pEnhanced);
+	registerBoolSetting("Enhance destroy", &dEnhanced, dEnhanced);
+	delay = 0;
 }
-
 CrystalAura::~CrystalAura() {
 }
-
 const char* CrystalAura::getModuleName() {
 	return ("CrystalAura");
 }
 
+static std::vector<C_Entity*> targetList;
+
 void CrystalAura::onEnable() {
+	targetList.clear();
 	this->delay = 0;
 }
 
-void CrystalAura::onTick(C_GameMode* gm) {
-	
-	this->delay++;
-	if (this->delay >= 5) {
-		this->delay = 0;
+void CfindEntity(C_Entity* curEnt, bool isRegularEntity) {
+	if (curEnt == nullptr)
+		return;
+	if (curEnt == g_Data.getLocalPlayer())  // Skip Local player
+		return;
+	if (!curEnt->isAlive())
+		return;
+	if (!g_Data.getLocalPlayer()->isAlive())
+		return;
+	if (!g_Data.getLocalPlayer()->canAttack(curEnt, false))
+		return;;
+	//if (Target::VanillaAttac(curEnt, true))
+	//	return;
+	if (curEnt->getEntityTypeId() == 71) // endcrystal
+		return;
+	if (curEnt->getEntityTypeId() == 66) // falling block
+		return;
+	if (!Target::isValidTarget(curEnt))
+		return;
+	float dist = (*curEnt->getPos()).dist(*g_Data.getLocalPlayer()->getPos());
+	if (dist <= moduleMgr->getModule<CrystalAura>()->pRange) {
+		targetList.push_back(curEnt);
+	}
+}
 
-		if (autoplace) {
-			vec3_t* pos = gm->player->getPos();
-			for (int x = (int)pos->x - range; x < pos->x + range; x++) {
-				for (int z = (int)pos->z - range; z < pos->z + range; z++) {
-					for (int y = (int)pos->y - range; y < pos->y + range; y++) {
-						vec3_ti blockPos = vec3_ti(x, y, z);
-						vec3_ti upperBlockPos = vec3_ti(x, y + 1, z);
-						C_Block* block = gm->player->region->getBlock(blockPos);
-						C_Block* upperBlock = gm->player->region->getBlock(upperBlockPos);
-						if (block != nullptr) {
-							int blockId = block->toLegacy()->blockId;
-							int upperBlockId = upperBlock->toLegacy()->blockId;
-							if ((blockId == 49 || blockId == 7) && upperBlockId == 0) {
-								gm->buildBlock(&blockPos, 0);
-							}
-						}
+void CrystalAura::CPlace(C_GameMode* gm, vec3_t* pos) {
+	for (int x = (int)pos->x - eRange; x < pos->x + eRange; x++) {
+		for (int z = (int)pos->z - eRange; z < pos->z + eRange; z++) {
+			for (int y = (int)pos->y - eRange; y < pos->y + eRange; y++) {
+				vec3_ti blockPos = vec3_ti(x, y, z);
+				vec3_ti upperBlockPos = vec3_ti(x, y + 1, z);
+				C_Block* block = gm->player->region->getBlock(blockPos);
+				C_Block* upperBlock = gm->player->region->getBlock(upperBlockPos);
+				if (block != nullptr) {
+					int blockId = block->toLegacy()->blockId;
+					int upperBlockId = upperBlock->toLegacy()->blockId;
+					if ((blockId == 49 || blockId == 7) && upperBlockId == 0//Check for awailable block 
+						/* && !(x == (int)pos->x && z == (int)pos->z && y == (int)pos->y)*/){ // don't place in entety
+						gm->buildBlock(&blockPos, 0);
+							return;
 					}
 				}
 			}
 		}
+	}
+}
 
-		g_Data.forEachEntity([](C_Entity* ent, bool b) {
-			int id = ent->getEntityTypeId();
-			int range = moduleMgr->getModule<CrystalAura>()->range;
-			if (id == 71 && g_Data.getLocalPlayer()->getPos()->dist(*ent->getPos()) <= range) {
+void CrystalAura::CDestroy(C_Entity* ent) {
+	int id = ent->getEntityTypeId();
+	if (id == 71 && g_Data.getLocalPlayer()->getPos()->dist(*ent->getPos()) < cRange && !dEnhanced) {
+		g_Data.getCGameMode()->attack(ent);
+		if (!moduleMgr->getModule<NoSwing>()->isEnabled())
+			g_Data.getLocalPlayer()->swingArm();
+	} else if (id == 71 && dEnhanced) {
+		for (auto& i : targetList)
+			if (ent->getPos()->dist(*i->getPos()) < cRange) {
 				g_Data.getCGameMode()->attack(ent);
-				if (!moduleMgr->getModule<NoSwing>()->isEnabled()) 
+				if (!moduleMgr->getModule<NoSwing>()->isEnabled())
 					g_Data.getLocalPlayer()->swingArm();
+				return;
 			}
+	}
+}
+
+void CrystalAura::onTick(C_GameMode* gm) {
+	this->delay++;
+	targetList.clear();
+	g_Data.forEachEntity(CfindEntity);
+	if (this->delay >= 6) {
+		this->delay = 0;
+		if (autoplace && g_Data.getLocalPlayer()->getSelectedItemId() == 0x1aa) {//endcrystal
+			if (pEnhanced)
+				for (auto& i : targetList)
+					CPlace(gm, i->getPos());
+			else
+				CPlace(gm, gm->player->getPos());
+		}
+		g_Data.forEachEntity([](C_Entity* ent, bool b) {
+			moduleMgr->getModule<CrystalAura>()->CDestroy(ent);
 		});
 	}
 }
--- Horion/Module/Modules/CrystalAura.h
+++ Horion/Module/Modules/CrystalAura.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Module.h"
 #include "../ModuleManager.h"
+#include "../../../Utils/Target.h"
 
 class CrystalAura : public IModule {
 private:
 	int delay = 0;
-	bool autoplace = true;
+	int eRange = 3;
+	int range = 10;
+	bool autoplace = false;
+	bool pEnhanced = true;
+	bool dEnhanced = true;
 
 public:
 	CrystalAura();
@@ -16,5 +21,10 @@ public:
 	virtual void onTick(C_GameMode* gm) override;
 	virtual void onEnable() override;
 	virtual void onDisable() override;
-	int range = 5;
+
+	int cRange = 10;
+	int pRange = 5;
+
+	void CPlace(C_GameMode* gm, vec3_t* pos);
+	void CDestroy(C_Entity* ent);
 };
