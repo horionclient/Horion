70a2bbe0dca1074ebe20062c0f83d110aa9a0b51
erwan celanie <ti-yoyo789@hotmail.fr>
2020-12-28 11:24:20 -0400

    fix nbt

--- SDK/CBlockLegacy.cpp
+++ SDK/CBlockLegacy.cpp
@@ -9,7 +9,7 @@ C_Block* C_BlockSource::getBlock(const vec3_ti& block) {
 }
 C_BlockActor* C_BlockSource::getBlockEntity(const vec3_ti& block) {
 	using getBlockEntity_t = C_BlockActor*(__fastcall*)(C_BlockSource*, const vec3_ti&);
-	static getBlockEntity_t getBlockEntity = reinterpret_cast<getBlockEntity_t>(FindSignature("40 53 48 83 EC ?? 8B 02 48 8B DA C1 F8 ?? 89 44 24 ?? 8B 42 ?? 48 8D 54 24 ? C1 F8 04 89 44 24 ?? E8 ? ? ? ? 48 85 C0 74 31"));
+	static getBlockEntity_t getBlockEntity = reinterpret_cast<getBlockEntity_t>(FindSignature("48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC ? 8B 02 48 8B DA C1 F8 ? 48 8B F1 89 44 24 ? 8B 42"));
 	return getBlockEntity(this, block);
 }
 C_Block* C_BlockSource::getLiquidBlock(const vec3_ti& block) {
