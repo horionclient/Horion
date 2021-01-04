3626deffb22c7c567d9e4834dd75ec26f361c563
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-01-04 18:47:09 +0100

    Now we got modes Boys

--- Horion/Module/Modules/TestModule.cpp
+++ Horion/Module/Modules/TestModule.cpp
@@ -7,8 +7,8 @@
 #include <array>
 #include <unordered_set>
 #include <glm/mat4x4.hpp>
-#include <glm/trigonometric.hpp>  //radians
-#include <glm/ext/matrix_transform.hpp> // perspective, translate, rotate
+#include <glm/trigonometric.hpp>			//radians
+#include <glm/ext/matrix_transform.hpp>		// perspective, translate, rotate
 #include <glm/ext/matrix_relational.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/gtc/constants.hpp>
@@ -20,7 +20,12 @@ using json = nlohmann::json;
 
 
 TestModule::TestModule() : IModule(0, Category::MISC, "For testing purposes") {
+	enum1 = *(*new SettingEnum(this)).addEntry(EnumEntry("1", 1)).addEntry(EnumEntry("2", 2))
+		.addEntry(EnumEntry("3", 3)).Enum;
 	registerFloatSetting("float1", &this->float1, 0, -10, 10);
+	registerIntSetting("int1", &this->int1, 0, -10, 10);
+	registerEnumSetting("Enum1", &enum1, 0);
+	registerBoolSetting("bool1", &this->bool1, true);
 }
 
 TestModule::~TestModule() {
