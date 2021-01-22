62a668cec4b7187859ec0ea402986359a810adce
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-01-21 23:29:40 -0600

    Add files via upload

--- Horion/Module/Modules/TestModule.h
+++ Horion/Module/Modules/TestModule.h
@@ -2,22 +2,18 @@
 #include "Module.h"
 class TestModule : public IModule {
 private:
-	int delay = 0;
+	float playersize = 1;
+	bool active = false;
+	float ogsize = 0;
 
 public:
-	float float1 = 0;
-
+	
 	TestModule();
 	~TestModule();
 
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
-	virtual bool isFlashMode() override;
 	virtual void onEnable() override;
 	virtual void onTick(C_GameMode* gm) override;
-	virtual void onPostRender(C_MinecraftUIRenderContext* renderCtx) override;
-	virtual void onSendPacket(C_Packet* p) override;
-	virtual void onMove(C_MoveInputHandler* hand) override;
 	virtual void onDisable() override;
-	void onLevelRender() override;
 };
