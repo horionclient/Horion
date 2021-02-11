8046040106a67d22f2061aca3ab09f77cb6195a3
Not Existing <61290107+N0tExisting@users.noreply.github.com>
2021-02-11 22:28:04 +0100

    Better crystal aura (#4366) untested code
    * Added Better CrystalAura
    
    * Last Minute Fix
    
    Why do i always forget this
    
    * Improved Crystal Aura
    
    * Added Preview & stuff
    
    * Fix
    
    fixed crash when exiting worlds
    fixed usage of wrong constructor(hopefuly)

--- Horion/Module/Modules/CrystalAura.h
+++ Horion/Module/Modules/CrystalAura.h
@@ -1,11 +1,21 @@
 #pragma once
-#include "Module.h"
+#include "../../../Utils/Target.h"
 #include "../ModuleManager.h"
+#include "Module.h"
 
 class CrystalAura : public IModule {
 private:
+	int prevSlot;
 	int delay = 0;
+	int eRange = 3;
 	bool autoplace = true;
+	bool pEnhanced = true;
+	bool dEnhanced = true;
+	bool Preview = true;
+	bool AutoSelect = true;
+	bool FinishSelect = false;
+	C_PlayerInventoryProxy* supplies = nullptr;
+	C_Inventory* inv = nullptr;
 
 public:
 	CrystalAura();
@@ -14,7 +24,14 @@ public:
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
+	virtual void onPreRender(C_MinecraftUIRenderContext* renderCtx) override;
 	virtual void onEnable() override;
 	virtual void onDisable() override;
-	int range = 5;
+
+	int cRange = 10;
+	int pRange = 5;
+	int range = 10;
+
+	void CPlace(C_GameMode* gm, vec3_t* pos);
+	void DestroyC(C_Entity* ent, int range);
 };
