ede41d4f9d9dff19a954edd4ac6dbab11aecabd2
godgamer029 <godgamer029@gmail.com>
2021-02-11 22:32:44 +0100

    Implement Fullbright suggested by N0tExisting

--- Horion/Module/Modules/FullBright.cpp
+++ Horion/Module/Modules/FullBright.cpp
@@ -6,6 +6,8 @@ FullBright::FullBright() : IModule(0, Category::VISUAL, "Puts your gamma to max"
 FullBright::~FullBright() {
 }
 
+float originalGamma = -1;
+
 const char* FullBright::getModuleName() {
 	return "Fullbright";
 }
@@ -16,11 +18,18 @@ void FullBright::onTick(C_GameMode* gm) {
 }
 
 void FullBright::onEnable() {
-	if (gammaPtr != nullptr)
+	if (gammaPtr != nullptr) {
+		originalGamma = *gammaPtr;
 		*gammaPtr = 10;
+	}
 }
 
 void FullBright::onDisable() {
-	if (gammaPtr != nullptr)
-		*gammaPtr = 0.5f;
+	if (gammaPtr != nullptr) {
+		if (originalGamma >= 0 && originalGamma <= 1)
+			*gammaPtr = originalGamma;
+		else
+			*gammaPtr = 0.5f;
+	}
+		
 }
