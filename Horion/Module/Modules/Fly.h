13d7d43bd88c01fb4ec476fed8b711f002d949d3
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-26 16:34:19 -0600

    made CubeGlide a mode of fly

--- Horion/Module/Modules/Fly.h
+++ Horion/Module/Modules/Fly.h
@@ -1,11 +1,19 @@
 #pragma once
 #include "Module.h"
 class Fly : public IModule {
+private:
+	float speed = 1.5f;
+	int gameTick = 0;
+
 public:
 	Fly();
 	~Fly();
 
+	SettingEnum mode;
+
 	// Inherited via IModule
+	virtual bool isFlashMode() override;
+	virtual void onEnable() override;
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
 	virtual void onDisable() override;
