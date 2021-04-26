dbeea1ebfb65dc5161ff5cc15e0c6c7d08a1d56c
szeroki <36236750+Seyz123@users.noreply.github.com>
2021-04-26 20:05:08 +0200

    Reach fix

--- Horion/Module/Modules/Reach.cpp
+++ Horion/Module/Modules/Reach.cpp
@@ -25,7 +25,7 @@ void Reach::onEnable() {
 	static uintptr_t sigOffset = 0x0;
 	if (sigOffset == 0x0) {
 
-		sigOffset = FindSignature("F3 0F 10 05 ?? ?? ?? ?? 44 0F 2F E0 ??");
+		sigOffset = FindSignature("F3 0F 59 ? ? ? ? ? 80 BE");
 
 		if (sigOffset != 0x0) {
 			int offset = *reinterpret_cast<int*>((sigOffset + 4));  // Get Offset from code
