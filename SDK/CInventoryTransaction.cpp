a258203fdcf6b99b5c40b5a2bc7de962c6c55f26
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-08-09 21:03:29 -0400

    Update CInventoryTransaction.cpp

--- SDK/CInventoryTransaction.cpp
+++ SDK/CInventoryTransaction.cpp
@@ -1,5 +1,6 @@
 #include "CInventoryTransaction.h"
 #include "../Utils/Utils.h"
+//InventoryTransactionManager::addAction(InventoryAction const &,bool)
 void C_InventoryTransactionManager::addInventoryAction(const C_InventoryAction& action,bool idk) {
 	using InventoryTransactionManager__addAction_t = void(__fastcall*)(C_InventoryTransactionManager*, C_InventoryAction const&,bool);
 	//static auto InventoryTransactionManager__addAction = reinterpret_cast<InventoryTransactionManager__addAction_t>(FindSignature("40 55 56 57 41 56 41 57 48 83 EC 30 48 ?? ?? ?? ?? ?? ?? ?? ?? 48 89 5C 24 ?? 48 8B EA 4C 8B F1 4C 8B C2 48 8B 51 ?? 48 8B 49 ?? E8"));
