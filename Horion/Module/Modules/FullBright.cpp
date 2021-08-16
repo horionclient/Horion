8546520eecb5353f1559bd6010debdd51dde0b70
Packet <86270028+PacketDeveloper@users.noreply.github.com>
2021-08-16 12:25:54 -0600

    Update FullBright.cpp (#4417)

--- Horion/Module/Modules/FullBright.cpp
+++ Horion/Module/Modules/FullBright.cpp
@@ -1,7 +1,7 @@
 #include "FullBright.h"
 
 FullBright::FullBright() : IModule(0, Category::VISUAL, "Puts your gamma to max") {
-	registerFloatSetting("Intensity", &this->intensity, this->intensity, -100.f, 10.f);
+	registerFloatSetting("Intensity", &this->intensity, this->intensity, -100.f, 25.f);
 }
 
 FullBright::~FullBright() {
