c0fc3aa3365ff217b82225598f599e9f8a32f102
rxversed <70835203+rxversedcode@users.noreply.github.com>
2021-08-16 09:32:42 -0800

    Update Freecam.h

--- Horion/Module/Modules/Freecam.h
+++ Horion/Module/Modules/Freecam.h
@@ -3,6 +3,7 @@
 class Freecam : public IModule {
 private:
 	vec3_t oldPos;
+	float speed = 0.325f;
 
 public:
 	Freecam();
@@ -11,6 +12,7 @@ public:
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
+	virtual void onMove(C_MoveInputHandler* input) override;
 	virtual void onEnable() override;
 	virtual void onDisable() override;
 };
