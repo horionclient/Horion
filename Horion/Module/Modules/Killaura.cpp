8565f100c1ccabfacce43c7566cf0797f078ef44
rxversed <70835203+rxversedcode@users.noreply.github.com>
2021-08-16 11:31:03 -0800

    Update Killaura.cpp

--- Horion/Module/Modules/Killaura.cpp
+++ Horion/Module/Modules/Killaura.cpp
@@ -128,23 +128,29 @@ void Killaura::onSendPacket(C_Packet* packet) {
 
 void Killaura::onPostRender(C_MinecraftUIRenderContext* ctx) {
 	if (!targetList.empty() && targetHUD && g_Data.isInGame()) {
-		static float rcolors[4];          // Rainbow color array RGBA
-		static float disabledRcolors[4];  // Rainbow Colors, but for disabled modules
-		static float currColor[4];        // ArrayList colors
-	
+		vec4_t tempPos = vec4_t(70.f, 70.f, 50.f, 40.f); //temp pos for the text pos, so we can create a pos that doesn't have player names overlapping from the box to the screen
+		vec2_t textPos = vec2_t(tempPos.y, tempPos.x); //text pos
+		vec4_t pos = vec4_t(70.f, 70.f, 100.f + textPos.x, 90.f); //pos for using
+		static float rcolors2[4];          // Rainbow color array RGBA
+		static float disabledRcolors2[4];  // Rainbow Colors, but for disabled modules
+		static float currColor[4];         // ArrayList colors
+
 		// Rainbow color updates
 		{
-			Utils::ApplyRainbow(rcolors);  // Increase Hue of rainbow color array
-			disabledRcolors[0] = std::min(1.f, rcolors[0] * 0.4f + 0.2f);
-			disabledRcolors[1] = std::min(1.f, rcolors[1] * 0.4f + 0.2f);
-			disabledRcolors[2] = std::min(1.f, rcolors[2] * 0.4f + 0.2f);
-			disabledRcolors[3] = 1;
+			Utils::ApplyRainbow(rcolors2, 0.001f);  // Increase Hue of rainbow color array
+			disabledRcolors2[0] = std::min(1.f, rcolors2[0] * 0.4f + 0.2f);
+			disabledRcolors2[1] = std::min(1.f, rcolors2[1] * 0.4f + 0.2f);
+			disabledRcolors2[2] = std::min(1.f, rcolors2[2] * 0.4f + 0.2f);
+			disabledRcolors2[3] = 1;
 		}
+		currColor[3] = rcolors2[3];
+		Utils::ColorConvertRGBtoHSV(rcolors2[0], rcolors2[1], rcolors2[2], currColor[0], currColor[2], currColor[2]);
+		Utils::ColorConvertHSVtoRGB(currColor[0], currColor[2], currColor[3], currColor[0], currColor[0], currColor[1]);
 		std::string name = targetList[0]->getNameTag()->getText();
 		std::string distance = "Distance: " + std::to_string((*targetList[0]->getPos()).dist(*g_Data.getLocalPlayer()->getPos()));
 
-		DrawUtils::drawRectangle(pos, rcolors, 1.f);
-		DrawUtils::fillRectangle(pos, MC_Color(13, 29, 48), 1.f);
+		DrawUtils::drawRectangle(pos, currColor, 1.f);
+		DrawUtils::fillRectangle(pos, MC_Color(0, 0, 0), 1.f);
 		DrawUtils::drawText(textPos, &name, currColor, 1.f);
 		textPos.y += 10.f;
 		DrawUtils::drawText(textPos, &distance, currColor, 1.f);
