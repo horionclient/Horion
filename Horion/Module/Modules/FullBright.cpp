bc20d86f7a8cf8e37cdd876e5add5921f5be30a9
Turkeii <81539722+Turkeii@users.noreply.github.com>
2021-08-17 17:06:14 -0500

    Update FullBright.cpp

--- Horion/Module/Modules/FullBright.cpp
+++ Horion/Module/Modules/FullBright.cpp
@@ -1,36 +1,27 @@
 #include "FullBright.h"
 
-FullBright::FullBright() : IModule(0, Category::VISUAL, "Puts your gamma to max") {
-	registerFloatSetting("Intensity", &this->intensity, this->intensity, -100.f, 25.f);
+FullBright::FullBright() : IModule(0, Category::VISUAL, "increases your gamma") {
+	this->registerFloatSetting("Gamma", &this->gamma, this->gamma, 0.25f, 10.f);
 }
 
 FullBright::~FullBright() {
 }
-
-float originalGamma = -1;
 
 const char* FullBright::getModuleName() {
 	return "Fullbright";
 }
 
 void FullBright::onTick(C_GameMode* gm) {
-	if (gammaPtr != nullptr && *gammaPtr != 10)
-		*gammaPtr = 10;
+	if (gammaPtr != nullptr)
+		*gammaPtr = gamma;
 }
 
 void FullBright::onEnable() {
-	if (gammaPtr != nullptr) {
-		originalGamma = *gammaPtr;
-		*gammaPtr = 10;
-	}
+	if (gammaPtr != nullptr)
+		*gammaPtr = gamma;
 }
 
 void FullBright::onDisable() {
-	if (gammaPtr != nullptr) {
-		if (originalGamma >= 0 && originalGamma <= 1)
-			*gammaPtr = originalGamma;
-		else
-			*gammaPtr = 0.5f;
-	}
-		
+	if (gammaPtr != nullptr)
+		*gammaPtr = 0.5f;
 }
