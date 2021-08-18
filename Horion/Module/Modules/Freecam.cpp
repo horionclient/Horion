4d339ba5e8d7ed55b18b3c0c6b746b69c9da0227
thesynx <87203897+Synx173@users.noreply.github.com>
2021-08-18 16:41:50 -0400

    fix freecam being troll
    bruh moment

--- Horion/Module/Modules/Freecam.cpp
+++ Horion/Module/Modules/Freecam.cpp
@@ -14,46 +14,46 @@ const char* Freecam::getModuleName() {
 void Freecam::onTick(C_GameMode* gm) {
 	gm->player->fallDistance = 0.f;
 	gm->player->velocity = vec3_t(0, 0, 0);
+	gm->player->aabb.upper = gm->player->aabb.lower;
 }
 
 void Freecam::onEnable() {
 	if (g_Data.getLocalPlayer() != nullptr) {
 		oldPos = *g_Data.getLocalPlayer()->getPos();
+		oldOffset = g_Data.getLocalPlayer()->aabb.upper.sub(g_Data.getLocalPlayer()->aabb.lower);
 	}
 }
 
 void Freecam::onMove(C_MoveInputHandler* input) {
-	auto player = g_Data.getLocalPlayer();  
-	if (player == nullptr) return;          
+	auto player = g_Data.getLocalPlayer();
+	if (player == nullptr) return;
 
-	
+	vec2_t moveVec2d = {input->forwardMovement, -input->sideMovement};
+	bool pressed = moveVec2d.magnitude() > 0.01f;
 
-	vec2_t moveVec2d = { input->forwardMovement, -input->sideMovement };  
-	bool pressed = moveVec2d.magnitude() > 0.01f;                       
-
-	float calcYaw = (player->yaw + 90) * (PI / 180);                                    
-	vec3_t moveVec;                                                                      
-	float c = cos(calcYaw);                                                              
-	float s = sin(calcYaw);                                                              
-	moveVec2d = { moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c };  
-	moveVec.x = moveVec2d.x * speed;                                                     
-	moveVec.y = player->velocity.y;                                                      
-	moveVec.z = moveVec2d.y * speed;                                                    
-	if (pressed) player->lerpMotion(moveVec);                                            
-	C_MovePlayerPacket p(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());   
+	float calcYaw = (player->yaw + 90) * (PI / 180);
+	vec3_t moveVec;
+	float c = cos(calcYaw);
+	float s = sin(calcYaw);
+	moveVec2d = {moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c};
+	moveVec.x = moveVec2d.x * speed;
+	moveVec.y = player->velocity.y;
+	moveVec.z = moveVec2d.y * speed;
+	if (pressed) player->lerpMotion(moveVec);
+	C_MovePlayerPacket p(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
 	if (input->isJumping) {
 		player->velocity.y += 0.50f;
-		
 	}
 	if (input->isSneakDown) {
 		player->velocity.y -= 0.50f;
-		
 	}
 }
 
 void Freecam::onDisable() {
-	if (g_Data.getLocalPlayer() != nullptr) {
-		g_Data.getLocalPlayer()->setPos(oldPos);
+	auto plr = g_Data.getLocalPlayer();
+	if (plr) {
+		plr->setPos(oldPos);
 		*g_Data.getClientInstance()->minecraft->timer = 20.f;
+		plr->aabb.upper = plr->aabb.lower.add(oldOffset);
 	}
 }
