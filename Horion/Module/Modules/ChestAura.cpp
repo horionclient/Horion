b1498c101c6fe3d335dc4da63e591c66212bb636
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-09-06 15:25:45 -0400

    u saw nothing

--- Horion/Module/Modules/ChestAura.cpp
+++ Horion/Module/Modules/ChestAura.cpp
@@ -15,7 +15,7 @@ const char* ChestAura::getModuleName() {
 void ChestAura::onTick(C_GameMode* gm) {
 	if (g_Data.getLocalPlayer()->getSupplies()->inventory->isFull())
 		return;
-	
+
 	vec3_t* pos = gm->player->getPos();
 	for (int x = (int)pos->x - range; x < pos->x + range; x++) {
 		for (int z = (int)pos->z - range; z < pos->z + range; z++) {
@@ -25,19 +25,19 @@ void ChestAura::onTick(C_GameMode* gm) {
 				if (block != nullptr && g_Data.canUseMoveKeys()) {
 					auto id = gm->player->region->getBlock(pos)->toLegacy()->blockId;
 					bool open = false;
-					if (id == 54) open = true;   // Chests
+					if (id == 54) open = true;                  // Chests
 					if (id == 130 && enderchests) open = true;  // EnderCheats
 					if (open)
-							if (!(std::find(chestlist.begin(), chestlist.end(), pos) != chestlist.end())) {
-								gm->buildBlock(&pos, 0);
-								chestlist.push_back(pos);
-								return;
-							}
+						if (!(std::find(chestlist.begin(), chestlist.end(), pos) != chestlist.end())) {
+							gm->buildBlock(&pos, 0);
+							chestlist.push_back(pos);
+							return;
 						}
-					}
 				}
 			}
 		}
+	}
+}
 
 void ChestAura::onDisable() {
 	this->chestlist.clear();  // this code should be changed later, the chestlist has to be cleared when loading into a new world
