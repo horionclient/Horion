bad0128a6efb94dd63b15781af2cf0f9ece6a099
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-09-07 12:58:47 -0400

    Add Ender-Chests to ChestAura (#4502)

--- Horion/Module/Modules/ChestAura.cpp
+++ Horion/Module/Modules/ChestAura.cpp
@@ -2,6 +2,7 @@
 
 ChestAura::ChestAura() : IModule(0, Category::PLAYER, "Aura for opening chests") {
 	registerIntSetting("Range", &this->range, this->range, 1, 10);
+	registerBoolSetting("EnderChests", &this->enderchests, this->enderchests);
 }
 
 ChestAura::~ChestAura() {
@@ -22,13 +23,16 @@ void ChestAura::onTick(C_GameMode* gm) {
 				vec3_ti pos = vec3_ti(x, y, z);
 				C_Block* block = gm->player->region->getBlock(pos);
 				if (block != nullptr && g_Data.canUseMoveKeys()) {
-					if (block->toLegacy()->blockId == 54) {
+					auto id = gm->player->region->getBlock(pos)->toLegacy()->blockId;
+					bool open = false;
+					if (id == 54) open = true;                  // Chests
+					if (id == 130 && enderchests) open = true;  // EnderCheats
+					if (open)
 						if (!(std::find(chestlist.begin(), chestlist.end(), pos) != chestlist.end())) {
 							gm->buildBlock(&pos, 0);
 							chestlist.push_back(pos);
 							return;
 						}
-					}
 				}
 			}
 		}
