e4dd1e9103073574267f37bc85d84a84637d1e79
Dan <theclashingfritz@users.noreply.github.com>
2021-09-11 05:18:02 -0400

    Redo indents for GiveCommand,h

--- Horion/Command/Commands/GiveCommand.h
+++ Horion/Command/Commands/GiveCommand.h
@@ -8,6 +8,6 @@ public:
 	// Inherited via IMCCommand
 	virtual bool execute(std::vector<std::string>* args) override;
 private:
-    bool giveItem(uint8_t count, int itemId, uint8_t itemData, std::string &tag);
-    bool giveItem(uint8_t count, TextHolder &text, uint8_t itemData, std::string &tag);
+	bool giveItem(uint8_t count, int itemId, uint8_t itemData, std::string &tag);
+	bool giveItem(uint8_t count, TextHolder &text, uint8_t itemData, std::string &tag);
 };
