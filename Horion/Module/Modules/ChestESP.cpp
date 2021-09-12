f12cada5ea2a3fd586df229731d1de1429cce94c
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-09-12 10:45:32 -0400

    StorageESP

--- Horion/Module/Modules/ChestESP.cpp
+++ Horion/Module/Modules/ChestESP.cpp
@@ -35,6 +35,9 @@ void ChestESP::onPreRender(C_MinecraftUIRenderContext* renderCtx) {
 		if (storageID == 54) DrawUtils::setColor(1.f, 1.f, 1.f, math);                     // Normal Chest
 		if (storageID == 146) DrawUtils::setColor(.92f, .14f, .14f, math);                 // Trapped Chest
 		if (storageID == 130) DrawUtils::setColor(0.435294f, 0.215686f, 0.631372f, math);  // Ender Chest
+		if (storageID == 458) DrawUtils::setColor(0.62, 0.31, 0.04, math);                 // Barrel
+		if (storageID == 205) DrawUtils::setColor(.49f, .17f, .95f, math);                 // Undyed Shulker Box
+		if (storageID == 218) DrawUtils::setColor(.08f, .91f, .99f, math);                 // Shulker Box
 
 		DrawUtils::drawBox(chest.lower, chest.upper, (float)fmax(0.2f, 1 / (float)fmax(1, g_Data.getLocalPlayer()->eyePos0.dist(chest.lower))), true);  // Fancy math to give an illusion of good esp
 	}
