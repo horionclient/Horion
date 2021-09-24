e991a3b3975fa456be766b44a7a9b3c523b0e70c
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2021-09-23 22:18:23 -0600

    added selfhurt to the fly modules

--- Horion/Module/Modules/AirJump.cpp
+++ Horion/Module/Modules/AirJump.cpp
@@ -1,10 +1,18 @@
 #include "AirJump.h"
 
 AirJump::AirJump() : IModule('G', Category::MOVEMENT, "Jump even you're not on the ground") {
+	registerBoolSetting("Selfhurt", &this->selfhurt, this->selfhurt);
 	registerBoolSetting("Legacy", &legacyMode, legacyMode);
 }
 
 AirJump::~AirJump() {
+}
+
+void AirJump::onEnable() {
+	if (selfhurt) {
+		g_Data.getLocalPlayer()->swing();
+		g_Data.getCGameMode()->attack(g_Data.getLocalPlayer());
+	}
 }
 
 const char* AirJump::getModuleName() {
