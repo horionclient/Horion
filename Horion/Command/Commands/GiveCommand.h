b36345aa45c34ce380a65f19ae6d6ea74f29e388
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2021-10-04 09:20:45 -0400

    Fixed .give nbt support. ex: .give dirt 1 0 1 (#4522)
    * Fixed .give nbt support. ex: .give dirt 1 0 1
    
    * Fixed .give nbt support. ex: .give dirt 1 0 1
    
    Fixed .give nbt support. ex: .give dirt 1 0 1. Credits to theclashingfritz for the multiple stack support pull.
    
    * Fixed .give nbt support. ex: .give dirt 1 0 1
    
    Fixed .give nbt support. ex: .give dirt 1 0 1. Credits to theclashingfritz for the multiple stack support pull.

--- Horion/Command/Commands/GiveCommand.h
+++ Horion/Command/Commands/GiveCommand.h
@@ -6,5 +6,9 @@ public:
 	~GiveCommand();
 
 	// Inherited via IMCCommand
-	virtual bool execute(std::vector<std::string>* args) override;
+	virtual bool execute(std::vector<std::string> *args) override;
+
+private:
+	bool giveItem(uint8_t count, int itemId, uint8_t itemData, std::string &tag);
+	bool giveItem(uint8_t count, TextHolder &text, uint8_t itemData, std::string &tag);
 };
