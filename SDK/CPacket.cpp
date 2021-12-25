56ac150b82e2a8802b5d94723c34a7046ff3c08d
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-12-25 14:21:04 -0500

    Fixed InventoryTransactionPacket sig

--- SDK/CPacket.cpp
+++ SDK/CPacket.cpp
@@ -69,7 +69,7 @@ PlayerAuthInputPacket::PlayerAuthInputPacket(__int64 entityRuntimeId, vec3_t pos
 C_MobEquipmentPacket::C_MobEquipmentPacket() {
 	static uintptr_t** MobEquipmentPacketVtable = 0x0;
 	if (MobEquipmentPacketVtable == 0x0) {
-		uintptr_t sigOffset = FindSignature("48 8D 0D ? ? ? ? 48 89 4D 90 48 89 45 B8 48 8D 97 ? ? ? ?");
+		uintptr_t sigOffset = FindSignature("48 8D 05 ? ? ? ? 48 89 51 28");
 		int offset = *reinterpret_cast<int*>(sigOffset + 3);
 		MobEquipmentPacketVtable = reinterpret_cast<uintptr_t**>(sigOffset + offset + /*length of instruction*/ 7);
 #ifdef _DEBUG
